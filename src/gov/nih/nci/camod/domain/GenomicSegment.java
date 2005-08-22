b2036df4f11ef679d9082b1f5616eb05bce66ff4
georgeda <georgeda>
2005-08-22 19:48:49 +0000

    Changed segment type to many to many
    SVN-Revision: 61

--- src/gov/nih/nci/camod/domain/GenomicSegment.java
+++ src/gov/nih/nci/camod/domain/GenomicSegment.java
@@ -6,70 +6,96 @@
  */
 package gov.nih.nci.camod.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 /**
  * @author rajputs
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * 
+ * TODO To change the template for this generated type comment go to Window -
+ * Preferences - Java - Code Style - Code Templates
  */
 public class GenomicSegment extends EngineeredGene {
 	private String locationOfIntegration;
+
 	private String segmentSize;
+
 	private String cloneDesignator;
-	private SegmentType segmentType;	
-	
+
+	private List segmentTypeCollection = new ArrayList();
+
+	/**
+	 * @return Returns the segmentTypeCollection.
+	 */
+	public List getSegmentTypeCollection() {
+		return segmentTypeCollection;
+	}
+
+	/**
+	 * @param segmentTypeCollection
+	 *            The segmentTypeCollection to set.
+	 */
+	public void setSegmentTypeCollection(List segmentTypeCollection) {
+		this.segmentTypeCollection = segmentTypeCollection;
+	}
+
+	/**
+	 * @param segmentType
+	 *            The segmentType to add.
+	 */
+	public void addSegmentType(SegmentType segmentType) {
+		segmentTypeCollection.add(segmentType);
+	}
+
 	/**
 	 * @return Returns the cloneDesignator.
 	 */
 	public String getCloneDesignator() {
 		return cloneDesignator;
 	}
+
 	/**
-	 * @param cloneDesignator The cloneDesignator to set.
+	 * @param cloneDesignator
+	 *            The cloneDesignator to set.
 	 */
 	public void setCloneDesignator(String cloneDesignator) {
 		this.cloneDesignator = cloneDesignator;
 	}
+
 	/**
 	 * @return Returns the locationOfIntegration.
 	 */
 	public String getLocationOfIntegration() {
 		return locationOfIntegration;
 	}
+
 	/**
-	 * @param locationOfIntegration The locationOfIntegration to set.
+	 * @param locationOfIntegration
+	 *            The locationOfIntegration to set.
 	 */
 	public void setLocationOfIntegration(String locationOfIntegration) {
 		this.locationOfIntegration = locationOfIntegration;
 	}
+
 	/**
 	 * @return Returns the segmentSize.
 	 */
 	public String getSegmentSize() {
 		return segmentSize;
 	}
+
 	/**
-	 * @param segmentSize The segmentSize to set.
+	 * @param segmentSize
+	 *            The segmentSize to set.
 	 */
 	public void setSegmentSize(String segmentSize) {
 		this.segmentSize = segmentSize;
 	}
-	/**
-	 * @return Returns the segmentType.
-	 */
-	public SegmentType getSegmentType() {
-		return segmentType;
-	}
-	/**
-	 * @param segmentType The segmentType to set.
-	 */
-	public void setSegmentType(SegmentType segmentType) {
-		this.segmentType = segmentType;
-	}
+
 	/**
 	 * @see java.lang.Object#equals(Object)
 	 */
@@ -79,27 +105,26 @@ public class GenomicSegment extends EngineeredGene {
 		}
 		GenomicSegment rhs = (GenomicSegment) object;
 		return new EqualsBuilder().appendSuper(super.equals(object)).append(
-				this.segmentType, rhs.segmentType).append(
 				this.locationOfIntegration, rhs.locationOfIntegration).append(
 				this.segmentSize, rhs.segmentSize).append(this.cloneDesignator,
 				rhs.cloneDesignator).isEquals();
 	}
+
 	/**
 	 * @see java.lang.Object#hashCode()
 	 */
 	public int hashCode() {
 		return new HashCodeBuilder(1738802159, 1516001829).appendSuper(
-				super.hashCode()).append(this.segmentType).append(
-				this.locationOfIntegration).append(this.segmentSize).append(
-				this.cloneDesignator).toHashCode();
+				super.hashCode()).append(this.locationOfIntegration).append(
+				this.segmentSize).append(this.cloneDesignator).toHashCode();
 	}
+
 	/**
 	 * @see java.lang.Object#toString()
 	 */
 	public String toString() {
 		return new ToStringBuilder(this).append("conditionality",
-				this.getConditionality()).append("segmentType",
-				this.segmentType).append("locationOfIntegration",
+				this.getConditionality()).append("locationOfIntegration",
 				this.locationOfIntegration).append("name", this.getName())
 				.append("id", this.getId()).append("comments",
 						this.getComments()).append("image", this.getImage())
