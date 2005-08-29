9e9045329002fbdee7cb68062f58f26e38f92370
georgeda <georgeda>
2005-08-29 14:32:35 +0000

    Changed checking to look for "protected"
    SVN-Revision: 121

--- src/gov/nih/nci/camod/util/CustomRequestProcessor.java
+++ src/gov/nih/nci/camod/util/CustomRequestProcessor.java
@@ -16,37 +16,30 @@ import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.RequestProcessor;
 
 /**
- * Checks for User Login on any struts request
- * Overrides the processActionPerform() method
+ * Checks for User Login on any struts request Overrides the
+ * processActionPerform() method
  * 
  * @author schroedlni
- *
+ * 
  */
 public class CustomRequestProcessor extends RequestProcessor {
-	
-	protected ActionForward processActionPerform( HttpServletRequest request, 
-												  HttpServletResponse response, 
-												  Action action, 
-												  ActionForm form, 
-												  ActionMapping mapping ) 
-	throws IOException, ServletException 
-	{	
-		System.out.println( "<CustomRequestProcessor processActionPerform> Entering..." );
-		System.out.println( "<CustomRequestProcessor processActionPerform> mapping.getParameter=" + mapping.getParameter() );
-		
-		// Check custom ActionMapping Parameter
-		if ( mapping.getParameter().toString().equals( "method") || mapping.getParameter().toString().indexOf("/protected") != -1 )
-		{	
-			
-			HttpSession session = request.getSession();
-			String user = (String) session.getAttribute( Constants.CURRENTUSER );
-					
-			if ( user == null ) {				
-				request.getSession().setAttribute( Constants.LOGINFAILED, "true" );				
-				return mapping.findForward( "login" );
-			}				
-		} 
 
-		return super.processActionPerform( request, response, action, form, mapping );
-	}
+    protected ActionForward processActionPerform(HttpServletRequest request, HttpServletResponse response,
+            Action action, ActionForm form, ActionMapping mapping) throws IOException, ServletException {
+
+
+        // Check custom ActionMapping Parameter
+        if (mapping.getParameter().toString().equals("method") || mapping.getParameter().toString().equals("protected")) {
+
+            HttpSession session = request.getSession();
+            String user = (String) session.getAttribute(Constants.CURRENTUSER);
+
+            if (user == null) {
+                request.getSession().setAttribute(Constants.LOGINFAILED, "true");
+                return mapping.findForward("login");
+            }
+        }
+
+        return super.processActionPerform(request, response, action, form, mapping);
+    }
 }
