632ce4ef29a838b2f67e2d850b3a281fe39a3b86
schroedn <schroedn>
2005-08-29 14:36:57 +0000

    Used for OM  meeting discussion
    SVN-Revision: 122

--- src/gov/nih/nci/camod/webapp/action/EnvironmentalFactorAction.java
+++ src/gov/nih/nci/camod/webapp/action/EnvironmentalFactorAction.java
@@ -7,13 +7,116 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionMessage;
 import org.apache.struts.action.ActionMessages;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import gov.nih.nci.camod.Constants;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.actions.DispatchAction;
+
+import gov.nih.nci.camod.webapp.form.EnvironmentalFactorForm;
+import gov.nih.nci.camod.webapp.form.ModelCharacteristicsForm;
+
+import gov.nih.nci.camod.domain.Person;
+import gov.nih.nci.camod.domain.EnvironmentalFactor;
+import gov.nih.nci.camod.domain.Phenotype;
+import gov.nih.nci.camod.domain.Treatment;
+import gov.nih.nci.camod.domain.SexDistribution;
+
+import gov.nih.nci.camod.service.AnimalModelManager;
+import gov.nih.nci.camod.domain.AnimalModel;
+import gov.nih.nci.camod.service.EnvironmentalFactorManager;
+
 
 /**
  * EnvironmentalFactorAction Class
  */
 public final class EnvironmentalFactorAction extends BaseAction {
+
+	/** Called from submitNewModel.jsp
+	 * 
+	 */ 
+    public ActionForward save( ActionMapping mapping, 
+    								   ActionForm form,
+							           HttpServletRequest request,
+							           HttpServletResponse response)
+    throws Exception {
+     
+    	EnvironmentalFactorForm envirFact = ( EnvironmentalFactorForm ) form;
+    	
+		System.out.println( "<EnvironmentalFactorAction save> following Characteristics:" + 
+																"\n\t name: " + envirFact.getName() + 
+																"\n\t otherName: " + envirFact.getOtherName() + 
+																"\n\t dosage: " + envirFact.getDosage() + 
+																"\n\t administrativeRoute: " + envirFact.getAdministrativeRoute() +
+																"\n\t regimen: " + envirFact.getRegimen() +
+																"\n\t ageAtTreatment: " + envirFact.getAgeAtTreatment() +
+																"\n\t type: " + envirFact.getType() +
+																"\n\t user: " + (String) request.getSession().getAttribute( "camod.loggedon.username" ) );
+		
+		Person person = new Person();
+		
+		//Set the current user as the person who entered this model
+		person.setUsername( (String) request.getSession().getAttribute( Constants.CURRENTUSER ) );    		    	
+			
+		/* EnvironmentalFactor Object */
+		EnvironmentalFactor environmentalFactor = new EnvironmentalFactor();		
+		environmentalFactor.setName(envirFact.getName());
+		
+		// if ( envirFact.getOtherName() != null || ! envirFact.getOtherName().equals( "" ) ) 
+		// {
+		//			System.out.println( " Someone entered in a new environmental name " + envirFact.getOtherName() );
+		//			System.out.println( " Sending a notice email to Ulli" );
+		// TODO: call a email function
+		//		}		
+		
+		/* Treatment Object */
+		Treatment treatment = new Treatment();
+		treatment.setDosage(envirFact.getDosage());
+		treatment.setRegimen(envirFact.getRegimen());
+		treatment.setAgeAtTreatment(envirFact.getAgeAtTreatment());		
+		treatment.setAdministrativeRoute(envirFact.getAdministrativeRoute());
+		
+		// if ( envirFact.getOtherAdministrativeRoute() != null || ! envirFact.getOtherAdministrativeRoute().equals( "" ) ) 
+		// {
+		//			System.out.println( " Someone entered in a new environmental name " + envirFact.getOtherAdministrativeRoute() );
+		//			System.out.println( " Sending a notice email to Ulli" );
+		// TODO: call a email function
+		//		}		
+		
+		/* SexDistribution Object */ 
+		SexDistribution sexDistribution = new SexDistribution();
+		sexDistribution.setType(envirFact.getType());
+		
+		EnvironmentalFactorManager environmentalFactorManager = (EnvironmentalFactorManager) getBean( "environmentalFactorManager" );
+		//environmentalFactorManager.save( person, environmentalFactor, treatment, sexDistribution );
+		environmentalFactorManager.save( environmentalFactor, treatment, sexDistribution ); 
+		
+		/*  Setup global constants to use for submission / editing process */
+		/*  Rework since save method does not return modelID */
+		//AnimalModel am = animalModelManager.get( "" + modelID );	      	
+    	//request.getSession().setAttribute( Constants.MODELID, am.getId().toString() );
+    	//request.getSession().setAttribute( Constants.MODELDESCRIPTOR, am.getModelDescriptor() );
+    	//request.getSession().setAttribute( Constants.MODELSTATUS, am.getState() );
+		
+    	//Add a message to be displayed in submitOverview.jsp saying you've created a new model successfully 
+        ActionMessages msg = new ActionMessages();
+        msg.add( ActionMessages.GLOBAL_MESSAGE, new ActionMessage( "creation.successful" ) );
+        saveErrors( request, msg );
+     
+        return mapping.findForward("submitEnvironmentalFactors");		
+    }	
 	
+
     /**
      * Delete
      * @param mapping
@@ -30,8 +133,61 @@ public final class EnvironmentalFactorAction extends BaseAction {
         if (log.isDebugEnabled()) {
             log.debug("Entering 'delete' method");
         }
+        
+    	EnvironmentalFactorForm envirFact = ( EnvironmentalFactorForm ) form;
+    	
+		System.out.println( "<EnvironmentalFactorAction save> following Characteristics:" + 
+																"\n\t name: " + envirFact.getName() + 
+																"\n\t otherName: " + envirFact.getOtherName() + 
+																"\n\t dosage: " + envirFact.getDosage() + 
+																"\n\t administrativeRoute: " + envirFact.getAdministrativeRoute() +
+																"\n\t regimen: " + envirFact.getRegimen() +
+																"\n\t ageAtTreatment: " + envirFact.getAgeAtTreatment() +
+																"\n\t type: " + envirFact.getType() +
+																"\n\t user: " + (String) request.getSession().getAttribute( "camod.loggedon.username" ) );
+		
+		Person person = new Person();
+		
+		//Set the current user as the person who entered this model
+		person.setUsername( (String) request.getSession().getAttribute( Constants.CURRENTUSER ) );    		    	
+			
+		//EnvironmentalFactor Object
+		EnvironmentalFactor environmentalFactor = new EnvironmentalFactor();
+		environmentalFactor.setName(envirFact.getName());
+		
+		// if ( envirFact.getOtherName() != null || ! envirFact.getOtherName().equals( "" ) ) 
+		// {
+		//			System.out.println( " Someone entered in a new environmental name " + envirFact.getOtherName() );
+		//			System.out.println( " Sending a notice email to Ulli" );
+		// TODO: call a email function
+		//		}		
+		
+		//Treatment Object
+		Treatment treatment = new Treatment();
+		treatment.setDosage(envirFact.getDosage());
+		treatment.setAdministrativeRoute(envirFact.getAdministrativeRoute());
+		treatment.setRegimen(envirFact.getRegimen());
+		treatment.setAgeAtTreatment(envirFact.getAgeAtTreatment());
+		
+		//SexDistribution Object to set gender 
+		SexDistribution sexDistribution = new SexDistribution();
+		sexDistribution.setType(envirFact.getType());
+		
+		EnvironmentalFactorManager environmentalFactorManager = (EnvironmentalFactorManager) getBean( "environmentalFactorManager" );
+		//environmentalFactorManager.save( person, environmentalFactor, treatment, sexDistribution ); 
+		
+		// Setup global constants to use for submission / editing process
+		//AnimalModel am = animalModelManager.get( "" + modelID );	      	
+    	//request.getSession().setAttribute( Constants.MODELID, am.getId().toString() );
+    	//request.getSession().setAttribute( Constants.MODELDESCRIPTOR, am.getModelDescriptor() );
+    	//request.getSession().setAttribute( Constants.MODELSTATUS, am.getState() );
+		
+    	//Add a message to be displayed in submitOverview.jsp saying you've created a new model successfully 
+        ActionMessages msg = new ActionMessages();
+        msg.add( ActionMessages.GLOBAL_MESSAGE, new ActionMessage( "creation.successful" ) );
+        saveErrors( request, msg );        
 
-        return mapping.findForward("");
+        return mapping.findForward( "submitEnvironmentalFactors" );
     }
 
 	/**
@@ -48,7 +204,7 @@ public final class EnvironmentalFactorAction extends BaseAction {
                                 HttpServletResponse response)
     throws Exception {
     	
-    	 return mapping.findForward("");
+    	 return mapping.findForward("submitEnvironmentalFactors");
     }    
     
     
@@ -65,30 +221,30 @@ public final class EnvironmentalFactorAction extends BaseAction {
                               HttpServletRequest request,
                               HttpServletResponse response)
     throws Exception {
-        if (log.isDebugEnabled()) {
-            log.debug("Entering 'edit' method");
-        }
 
-        return mapping.findForward("");
+    	// Grab the current modelID from the session  
+    	String modelID = (String) request.getSession().getAttribute( Constants.MODELID ); 
+    	
+    	EnvironmentalFactorForm envirFact = ( EnvironmentalFactorForm ) form;
+    	
+		System.out.println( "<EnvironmentalFactorAction save> following Characteristics:" + 
+																"\n\t name: " + envirFact.getName() + 
+																"\n\t otherName: " + envirFact.getOtherName() + 
+																"\n\t dosage: " + envirFact.getDosage() + 
+																"\n\t administrativeRoute: " + envirFact.getAdministrativeRoute() +
+																"\n\t regimen: " + envirFact.getRegimen() +
+																"\n\t ageAtTreatment: " + envirFact.getAgeAtTreatment() +
+																"\n\t type: " + envirFact.getType() +
+																"\n\t user: " + (String) request.getSession().getAttribute( "camod.loggedon.username" ) );
+		
+		EnvironmentalFactorManager environmentalFactorManager = (EnvironmentalFactorManager) getBean( "environmentalFactorManager" );
+		AnimalModelManager animalModelManager = (AnimalModelManager) getBean( "animalModelManager" );
+		
+		//retrieve model by it's id
+		AnimalModel animalModel = animalModelManager.get( modelID );   	
+
+        return mapping.findForward("submitEnvironmentalFactors");
     }
 
-    /**
-     * Save
-     * @param mapping
-     * @param form
-     * @param request
-     * @param response
-     * @return
-     * @throws Exception
-     */
-    public ActionForward save(ActionMapping mapping, ActionForm form,
-                              HttpServletRequest request,
-                              HttpServletResponse response)
-    throws Exception {
-        if (log.isDebugEnabled()) {
-            log.debug("Entering 'save' method");
-        }
 
-        return mapping.findForward("");
-    }
 }
--- src/gov/nih/nci/camod/webapp/action/EnvironmentalFactorPopulateAction.java
+++ src/gov/nih/nci/camod/webapp/action/EnvironmentalFactorPopulateAction.java
@@ -1,16 +1,27 @@
 package gov.nih.nci.camod.webapp.action;
 
+import java.util.List;
+
+import gov.nih.nci.camod.Constants;
+import gov.nih.nci.camod.domain.AnimalModel;
+import gov.nih.nci.camod.service.AnimalModelManager;
+import gov.nih.nci.camod.domain.EnvironmentalFactor;
+import gov.nih.nci.camod.service.EnvironmentalFactorManager;
+import gov.nih.nci.camod.webapp.form.EnvironmentalFactorForm;
+import gov.nih.nci.camod.webapp.util.DropdownUtil;
+
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-public class EnvironmentalFactorPopulateAction {
+public class EnvironmentalFactorPopulateAction extends BaseAction {
 
 	/** 
-	 * Pre-populate all field values in the form <FormName> 
-	 *  Used by <jspName>
+	 * Pre-populate all field values in the form EnvironmentalFactorForm 
+	 *  Used by submitEnvironmentalFactors
 	 * 
 	 */ 
 	public ActionForward populate( ActionMapping mapping, 
@@ -18,9 +29,98 @@ public class EnvironmentalFactorPopulateAction {
 						           HttpServletRequest request,
 						           HttpServletResponse response)
 	  throws Exception {
+		
+		System.out.println( "<EnvironmentalFactorPopulateAction populate> Entering... " );
 
-		return mapping.findForward("");
+		// Create a form to edit
+		EnvironmentalFactorForm envirFact = ( EnvironmentalFactorForm ) form;
+		
+		// Use the current animalModel based on the ID stored in the session
+		String modelID = "" + request.getSession().getAttribute( Constants.MODELID );		
+		AnimalModelManager animalModelManager = (AnimalModelManager) getBean( "animalModelManager" );
+		AnimalModel am = animalModelManager.get( modelID );	
+		
+		//TODO figure out how to deal with collection relationships
+		//envirFact.setName(am.getEnvironmentalFactorCollection().getClass().getName() );
 
-	}	
+		//envirFact.setDosage(am.);		
+		envirFact.setAdministrativeRoute("");
+		envirFact.setRegimen("");
+		envirFact.setAgeAtTreatment("");
+		
+		envirFact.setType( am.getPhenotype().getSexDistribution().getType());
+
+		//Store the Form in session to be used by the JSP
+		request.getSession().setAttribute( Constants.FORMDATA, envirFact );		
+
+		return mapping.findForward("submitEnvironmentalFactors");
+
+	}
 	
+	/**
+	 * Populate the dropdown menus for submitEnvironmentalFactors
+	 * 
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	public ActionForward dropdown( ActionMapping mapping, 
+			   					   ActionForm form,
+			   					   HttpServletRequest request,
+			   					   HttpServletResponse response )
+	  throws Exception {	
+		
+		System.out.println( "<EnvironmentalFactorPopulateAction dropdown> ... " );
+		
+		//setup dropdown menus
+		this.dropdown( request, response );
+		
+		return mapping.findForward( "submitEnvironmentalFactors" );
+	}	
+
+	/**
+	 * Populate all drowpdowns for this type of form 
+	 * 
+	 * @param request
+	 * @param response
+	 * @throws Exception
+	 */
+	public void dropdown( HttpServletRequest request,
+						  HttpServletResponse response )
+	  throws Exception {
+		
+			System.out.println( "<EnvironmentalFactorPopulateAction dropdown> Entering... " );
+		
+			//Prepopulate all dropdown fields, set the global Constants to the following
+			DropdownUtil drop = new DropdownUtil();
+			
+/*			TO DO - Implement dropdowns
+ 			//Environmental Factors dropdown
+			List envFactorsList = ;
+			
+			//Administrative Routes dropdown
+			//Dose units dropdown
+			
+			List ageLst = drop.getAgeList();
+			List sexDistList = drop.getSexDistributionsList();
+			
+			//TODO: Get specific list for speciesName ( for submitModelCharacteristics ) 
+			List strainList = drop.getStrainList( speciesList.get(0).toString() );
+			
+			ServletContext application = servlet.getServletConfig().getServletContext();			
+			String configFileName = application.getRealPath( "/config/SexDistributions.txt" );
+			
+			List sexDistList = drop.getDropdownListFromFile( configFileName );			 
+				
+			//Store the values for the drop down menus in a Constants variable, used in the JSP
+			request.getSession().setAttribute( Constants.ENVIRONFACTORDROP, envFactorList ); 
+		    request.getSession().setAttribute( Constants.SPECIESDROP, speciesStrainLst );
+			request.getSession().setAttribute( Constants.AGEDROP, ageLst );
+			request.getSession().setAttribute( Constants.SEXDISTRIBUTIONDROP, sexDistList );
+
+*/
+	}	
 }
