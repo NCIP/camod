3cd32e77fcee3ad8a56319cf64ba6457cf33fa2d
rajputs <rajputs>
2005-08-31 15:03:17 +0000

    New versions.
    SVN-Revision: 161

---
+++ src/gov/nih/nci/camod/service/TreatmentManager.java
@@ -0,0 +1,23 @@
+/*
+ * Created on Jun 17, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package gov.nih.nci.camod.service;
+
+import gov.nih.nci.camod.domain.Treatment;
+import java.util.List;
+
+/**
+ * @author rajputs
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public interface TreatmentManager {
+	public List getAll();
+	public Treatment get(String id);
+    public void save(Treatment treatment);
+    public void remove(String id);
+}
---
+++ src/gov/nih/nci/camod/service/impl/AgentManagerImpl.java
@@ -0,0 +1,76 @@
+/*
+ * Created on Jun 17, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package gov.nih.nci.camod.service.impl;
+
+import gov.nih.nci.camod.domain.Agent;
+import gov.nih.nci.camod.service.AgentManager;
+import gov.nih.nci.common.persistence.Persist;
+import gov.nih.nci.common.persistence.Search;
+import gov.nih.nci.common.persistence.exception.PersistenceException;
+import java.util.List;
+
+/**
+ * @author rajputs
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class AgentManagerImpl extends BaseManager implements AgentManager {
+	
+	public List getAll() {		
+		List agents = null;
+		
+		try {
+			agents = Search.query(Agent.class);
+		} catch (Exception e) {
+			System.out.println("Exception in AgentManagerImpl.getAll");
+			e.printStackTrace();
+		}
+		
+		return agents;
+	}
+	
+	public Agent get(String id) {
+		Agent agent = null;
+		
+		try {
+			agent = (Agent) Search.queryById(Agent.class, new Long(id));
+		} catch (PersistenceException pe) {
+			System.out.println("PersistenceException in AgentManagerImpl.get");
+			pe.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Exception in AgentManagerImpl.get");
+			e.printStackTrace();
+		}
+		
+		return agent;
+    }
+
+    public void save(Agent agent) {   
+    	try {
+			Persist.save(agent);			
+		} catch (PersistenceException pe) {
+			System.out.println("PersistenceException in AgentManagerImpl.save");
+			pe.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Exception in AgentManagerImpl.save");
+			e.printStackTrace();
+		}
+    }
+
+    public void remove(String id) {    
+    	try {
+			Persist.deleteById(Agent.class, new Long(id));
+		} catch (PersistenceException pe) {
+			System.out.println("PersistenceException in AgentManagerImpl.remove");
+			pe.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Exception in AgentManagerImpl.remove");
+			e.printStackTrace();
+		}
+    }
+}
