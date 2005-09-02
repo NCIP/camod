27189e20fcd46b70417e74ca2fc4c8cb1760b7eb
schroedn <schroedn>
2005-09-02 17:39:39 +0000

    Minor changes, added getStrainsList and a few debugging messages
    SVN-Revision: 178

--- src/gov/nih/nci/camod/webapp/util/NewDropdownUtil.java
+++ src/gov/nih/nci/camod/webapp/util/NewDropdownUtil.java
@@ -1,11 +1,21 @@
 package gov.nih.nci.camod.webapp.util;
 
 import gov.nih.nci.camod.Constants;
+import gov.nih.nci.camod.domain.Agent;
+import gov.nih.nci.camod.domain.Species;
+import gov.nih.nci.camod.domain.Strain;
 import gov.nih.nci.camod.domain.Taxon;
+import gov.nih.nci.camod.service.AgentManager;
 import gov.nih.nci.camod.service.TaxonManager;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -30,7 +40,10 @@ public class NewDropdownUtil {
             throw new IllegalArgumentException("Unknown dropdown list key: " + inDropdownKey);
         }
 
-        System.out.println("List: " + theList);
+        System.out.println("<NewDropdownUtil populateDropdown> inDropdownKey: " + inDropdownKey);
+        System.out.println("<NewDropdownUtil populateDropdown> inFilter: " + inFilter);
+        System.out.println("<NewDropdownUtil populateDropdown> List: " + theList);
+
         inRequest.setAttribute(inDropdownKey, theList);
     }
 
@@ -39,10 +52,18 @@ public class NewDropdownUtil {
         List theReturnList = null;
 
         // Grab them for the first time
-        if (inDropdownKey.equals(Constants.SPECIESDROP)) {
+        if (inDropdownKey.equals( Constants.Dropdowns.SPECIESDROP )) {
             theReturnList = getSpeciesList(inRequest);
         }
 
+        if (inDropdownKey.equals( Constants.Dropdowns.SURGERYDROP )) {
+            theReturnList = getSurgeryList(inRequest);
+        }
+        
+        if (inDropdownKey.equals( Constants.Dropdowns.STRAINDROP )) {
+            theReturnList = getStrainsList(inRequest, inFilter);
+        }
+        
         return theReturnList;
     }
 
@@ -120,15 +141,85 @@ public class NewDropdownUtil {
 
                 if (tmp.getScientificName() != null) {
                     // if the speciesName is not already in the List, add it
-                    // (only
-                    // get unique names)
+                    // (only get unique names)
                     if (!speciesNames.contains(tmp.getScientificName()))
                         speciesNames.add(tmp.getScientificName());
                 }
-            } // + " (" + tmp.getCommonName() + ")"
+            } 
         }
         Collections.sort(speciesNames);
 
         return speciesNames;
+    }
+    /**
+     * Based on a species name retrieve a list of all Strains
+     * 
+     * @param speciesName
+     * @return
+     */
+    private static List getStrainsList( HttpServletRequest inRequest, String speciesName ) {
+    	
+        TaxonManager taxonManager = ( TaxonManager) getContext(inRequest).getBean("taxonManager");
+
+        Species species = new Species();
+        species.setName(speciesName);
+
+        List strainList = new ArrayList();
+        List strainNames = new ArrayList();
+
+        strainList = taxonManager.getStrains(species);
+
+        if (strainList != null) {
+            // print out strainNames
+            for (int j = 0; j < strainList.size(); j++) {
+                Strain strain = (Strain) strainList.get(j);
+
+                if (strain.getName() != null) {
+                    strainNames.add(strain.getName());
+                    System.out.println("Strain Name>>" + j + ": " + strain.getName());
+                }
+            }
+        }
+
+        // Sort the list in 'abc' order
+        if (strainNames.size() > 0)
+            Collections.sort(strainNames);
+
+        strainNames.add("Other");
+
+        return strainNames;
+    }
+    
+    /**
+     * Returns a list of all types of surgeries
+     * 
+     */
+    private static List getSurgeryList(HttpServletRequest inRequest) {
+
+        AgentManager agentManager = (AgentManager) getContext(inRequest).getBean("agentManager");
+        
+        List agentList = agentManager.getAll();
+        List surgeryList = new ArrayList();
+        Agent tmp;
+
+        // TODO: Fix once we know what we're doing w/ this
+        surgeryList.add("Other");
+
+        if (agentList != null) {
+            for (int i = 0; i < agentList.size(); i++) {
+                tmp = (Agent) agentList.get(i);
+                
+                if (tmp.getName() != null) {
+                    // if the surgery is not already in the List, add it
+                    // (only get unique names)
+                    if ( !surgeryList.contains(tmp.getName()) && tmp.getType().equals( "Other" ) )
+                    		surgeryList.add(tmp.getName());
+                }
+            }
+        }
+            
+        Collections.sort(surgeryList);
+
+        return surgeryList;
     }
 }
