9aee3fb64f1b767273d940cf68f037955b998e9d
georgeda <georgeda>
2005-09-13 20:27:27 +0000

    Refactored and removed authorization code to UserManager
    SVN-Revision: 300

--- src/gov/nih/nci/camod/webapp/action/LoginAction.java
+++ src/gov/nih/nci/camod/webapp/action/LoginAction.java
@@ -10,8 +10,8 @@ package gov.nih.nci.camod.webapp.action;
 import gov.nih.nci.camod.Constants;
 import gov.nih.nci.camod.service.UserManager;
 import gov.nih.nci.camod.webapp.form.LoginForm;
-import gov.nih.nci.security.*;
-import gov.nih.nci.security.authorization.domainobjects.User;
+import gov.nih.nci.security.AuthenticationManager;
+import gov.nih.nci.security.SecurityServiceProvider;
 import gov.nih.nci.security.exceptions.CSException;
 
 import java.io.IOException;
@@ -21,16 +21,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.struts.action.*;
 
 public final class LoginAction extends BaseAction {
 
-    private static Log log = LogFactory.getLog(LoginAction.class);
-    // static final Logger log = Logger.getLogger(LoginAction.class.getName());
-    static AuthenticationManager authMgr = null;
-    static AuthorizationManager Mgr = null;
+    private static AuthenticationManager ourAuthorizationMgr = null;
 
     /**
      * Authenticates the user using the CSM AuthenticationManager and stores in
@@ -46,7 +41,6 @@ public final class LoginAction extends BaseAction {
         LoginForm loginForm = (LoginForm) form;
 
         log.debug("Logon Username: " + loginForm.getUsername());
-        log.debug("Logon Passwod: " + loginForm.getPassword());
         log.debug("System Config file is: " + System.getProperty("gov.nih.nci.security.configFile"));
 
         // check login credentials using Authentication Mangager
@@ -54,53 +48,29 @@ public final class LoginAction extends BaseAction {
 
         try {
             loginOK = getAuthenticationManager().login(loginForm.getUsername(), loginForm.getPassword());
-
             UserManager theUserManager = (UserManager) getBean("userManager");
             List theRoles = theUserManager.getRolesForUser(loginForm.getUsername());
             request.getSession().setAttribute(Constants.CURRENTUSERROLES, theRoles);
 
-        } catch (CSException ex) {
+        } catch (Exception ex) {
             loginOK = false;
             log.debug("The user was denied access to the csm application.", ex);
-            System.out.println("CSException " + ex);
         }
 
         String forward = Constants.FAILURE;
-        System.out.println("<LogonAction.java> execute: (authenticating...) username:" + loginForm.getUsername()
-                + " password:" + loginForm.getPassword());
 
         if (loginOK) {
-            System.out.println("<LogonAction.java> SUCESSFUL LOGIN ");
+            log.debug("Successful login");
             forward = "success";
             request.getSession().setAttribute(Constants.CURRENTUSER, loginForm.getUsername());
 
         } else {
-            System.out.println("<LogonAction.java> FAILURE LOGIN ");
+            log.debug("Failed login");
             forward = "login";
 
             ActionMessages errors = new ActionMessages();
             errors.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("errors.validation.header"));
             saveErrors(request, errors);
-        }
-
-        try {
-            AuthorizationManager Mgr = SecurityServiceProvider.getAuthorizationManager(Constants.UPT_CONTEXT_NAME);
-            User currentUser = Mgr.getUser(loginForm.getUsername());
-            currentUser.setPhoneNumber("555-555-5555");
-            currentUser.setFirstName("caMod");
-            currentUser.setLastName("Demo");
-            System.out.println("Username: " + currentUser.getUserId() + "\nFirstName: " + currentUser.getFirstName()
-                    + "\nLastName: " + currentUser.getLastName() + "\nPhoneNumber: " + currentUser.getPhoneNumber()
-                    + "\nPassword: " + currentUser.getPassword());
-
-            UserProvisioningManager Upm = (UserProvisioningManager) SecurityServiceProvider
-                    .getAuthorizationManager(Constants.UPT_CONTEXT_NAME);
-            Upm.modifyUser(currentUser);
-
-        } catch (CSException ex) {
-            System.out.println("CSException " + ex);
-        } catch (Throwable e) {
-            e.printStackTrace();
         }
 
         // Forward control to the specified success URI
@@ -112,17 +82,17 @@ public final class LoginAction extends BaseAction {
      * singleton pattern so that only one AuthenticationManager is created for
      * the CSM RI.
      * 
-     * @return
+     * @return the authentication manager instance
      * @throws CSException
      */
     protected AuthenticationManager getAuthenticationManager() throws CSException {
-        if (authMgr == null) {
+        if (ourAuthorizationMgr == null) {
             synchronized (LoginAction.class) {
-                if (authMgr == null) {
-                    authMgr = SecurityServiceProvider.getAuthenticationManager(Constants.UPT_CONTEXT_NAME);
+                if (ourAuthorizationMgr == null) {
+                    ourAuthorizationMgr = SecurityServiceProvider.getAuthenticationManager(Constants.UPT_CONTEXT_NAME);
                 }
             }
         }
-        return authMgr;
+        return ourAuthorizationMgr;
     }
 }
