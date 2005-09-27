c47af806bed54e27fc51a031d2c699bb7e4faddb
georgeda <georgeda>
2005-09-27 16:48:25 +0000

    Removed getAll - too many items in the DB
    SVN-Revision: 469

--- src/gov/nih/nci/camod/service/impl/TreatmentManagerImpl.java
+++ src/gov/nih/nci/camod/service/impl/TreatmentManagerImpl.java
@@ -11,66 +11,52 @@ import gov.nih.nci.camod.service.TreatmentManager;
 import gov.nih.nci.common.persistence.Persist;
 import gov.nih.nci.common.persistence.Search;
 import gov.nih.nci.common.persistence.exception.PersistenceException;
-import java.util.List;
 
 /**
  * @author rajputs
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * 
+ * TODO To change the template for this generated type comment go to Window -
+ * Preferences - Java - Code Style - Code Templates
  */
 public class TreatmentManagerImpl extends BaseManager implements TreatmentManager {
-	
-	public List getAll() {		
-		List treatments = null;
-		
-		try {
-			treatments = Search.query(Treatment.class);
-		} catch (Exception e) {
-			System.out.println("Exception in TreatmentManagerImpl.getAll");
-			e.printStackTrace();
-		}
-		
-		return treatments;
-	}
-	
-	public Treatment get(String id) {
-		Treatment treatment = null;
-		
-		try {
-			treatment = (Treatment) Search.queryById(Treatment.class, new Long(id));
-		} catch (PersistenceException pe) {
-			System.out.println("PersistenceException in TreatmentManagerImpl.get");
-			pe.printStackTrace();
-		} catch (Exception e) {
-			System.out.println("Exception in TreatmentManagerImpl.get");
-			e.printStackTrace();
-		}
-		
-		return treatment;
+
+    public Treatment get(String id) {
+        Treatment treatment = null;
+
+        try {
+            treatment = (Treatment) Search.queryById(Treatment.class, new Long(id));
+        } catch (PersistenceException pe) {
+            System.out.println("PersistenceException in TreatmentManagerImpl.get");
+            pe.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Exception in TreatmentManagerImpl.get");
+            e.printStackTrace();
+        }
+
+        return treatment;
     }
 
-    public void save(Treatment treatment) {   
-    	try {
-			Persist.save(treatment);			
-		} catch (PersistenceException pe) {
-			System.out.println("PersistenceException in TreatmentManagerImpl.save");
-			pe.printStackTrace();
-		} catch (Exception e) {
-			System.out.println("Exception in TreatmentManagerImpl.save");
-			e.printStackTrace();
-		}
+    public void save(Treatment treatment) {
+        try {
+            Persist.save(treatment);
+        } catch (PersistenceException pe) {
+            System.out.println("PersistenceException in TreatmentManagerImpl.save");
+            pe.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Exception in TreatmentManagerImpl.save");
+            e.printStackTrace();
+        }
     }
 
-    public void remove(String id) {    
-    	try {
-			Persist.deleteById(Treatment.class, new Long(id));
-		} catch (PersistenceException pe) {
-			System.out.println("PersistenceException in TreatmentManagerImpl.remove");
-			pe.printStackTrace();
-		} catch (Exception e) {
-			System.out.println("Exception in TreatmentManagerImpl.remove");
-			e.printStackTrace();
-		}
+    public void remove(String id) {
+        try {
+            Persist.deleteById(Treatment.class, new Long(id));
+        } catch (PersistenceException pe) {
+            System.out.println("PersistenceException in TreatmentManagerImpl.remove");
+            pe.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Exception in TreatmentManagerImpl.remove");
+            e.printStackTrace();
+        }
     }
 }
