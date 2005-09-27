faf30c612aff932d522d75e2cb4db577b76e1b5d
georgeda <georgeda>
2005-09-27 16:49:21 +0000

    Refactored w/ new manager design
    SVN-Revision: 471

--- src/gov/nih/nci/camod/service/impl/TherapyManagerImpl.java
+++ src/gov/nih/nci/camod/service/impl/TherapyManagerImpl.java
@@ -6,71 +6,106 @@
  */
 package gov.nih.nci.camod.service.impl;
 
-import gov.nih.nci.camod.domain.Therapy;
+import gov.nih.nci.camod.domain.*;
 import gov.nih.nci.camod.service.TherapyManager;
-import gov.nih.nci.common.persistence.Persist;
-import gov.nih.nci.common.persistence.Search;
-import gov.nih.nci.common.persistence.exception.PersistenceException;
+import gov.nih.nci.camod.webapp.form.ChemicalDrugData;
+
 import java.util.List;
 
 /**
  * @author rajputs
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * 
+ * TODO To change the template for this generated type comment go to Window -
+ * Preferences - Java - Code Style - Code Templates
  */
 public class TherapyManagerImpl extends BaseManager implements TherapyManager {
-	
-	public List getAll() {		
-		List therapies = null;
-		
-		try {
-			therapies = Search.query(Therapy.class);
-		} catch (Exception e) {
-			System.out.println("Exception in TherapyManagerImpl.getAll");
-			e.printStackTrace();
-		}
-		
-		return therapies;
-	}
-	
-	public Therapy get(String id) {
-		Therapy therapy = null;
-		
-		try {
-			therapy = (Therapy) Search.queryById(Therapy.class, new Long(id));
-		} catch (PersistenceException pe) {
-			System.out.println("PersistenceException in TherapyManagerImpl.get");
-			pe.printStackTrace();
-		} catch (Exception e) {
-			System.out.println("Exception in TherapyManagerImpl.get");
-			e.printStackTrace();
-		}
-		
-		return therapy;
+
+    public Therapy create(ChemicalDrugData inChemicalDrugData) {
+
+        log.trace("In TherapyManagerImpl.create");
+
+        Therapy theTherapy = new Therapy();
+        populateTherapy(inChemicalDrugData, theTherapy);
+
+        return theTherapy;
     }
 
-    public void save(Therapy therapy) {    	
-    	try {
-			Persist.save(therapy);			
-		} catch (PersistenceException pe) {
-			System.out.println("PersistenceException in TherapyManagerImpl.save");
-			pe.printStackTrace();
-		} catch (Exception e) {
-			System.out.println("Exception in TherapyManagerImpl.save");
-			e.printStackTrace();
-		}
+    public void update(ChemicalDrugData inChemicalDrugData, Therapy inTherapy) throws Exception {
+
+        log.trace("In TherapyManagerImpl.update");
+        populateTherapy(inChemicalDrugData, inTherapy);
+        save(inTherapy);
     }
 
-    public void remove(String id) {    	
-    	try {
-			Persist.deleteById(Therapy.class, new Long(id));
-		} catch (PersistenceException pe) {
-			System.out.println("PersistenceException in TherapyManagerImpl.remove");
-			pe.printStackTrace();
-		} catch (Exception e) {
-			System.out.println("Exception in TherapyManagerImpl.remove");
-			e.printStackTrace();
-		}
+    public List getAll() throws Exception {
+        log.trace("In TherapyManagerImpl.getAll");
+        return super.getAll(Therapy.class);
+    }
+
+    public Therapy get(String id) throws Exception {
+        log.trace("In TherapyManagerImpl.get");
+        return (Therapy) super.get(id, Therapy.class);
+    }
+
+    public void save(Therapy therapy) throws Exception {
+        log.trace("In TherapyManagerImpl.save");
+        super.save(therapy);
+    }
+
+    public void remove(String id) throws Exception {
+        log.trace("In TherapyManagerImpl.save");
+        super.remove(id, Therapy.class);
+    }
+
+    private void populateTherapy(ChemicalDrugData inChemicalDrug, Therapy theTherapy) {
+
+        // Set the treatment
+        Treatment theTreatment = theTherapy.getTreatment();
+        if (theTreatment == null) {
+            theTreatment = new Treatment();
+            theTherapy.setTreatment(theTreatment);
+        }
+
+        // Set the gender
+        SexDistribution sexDistribution = SexDistributionManagerSingleton.instance()
+                .getByType(inChemicalDrug.getType());
+
+        // save the treatment
+        theTreatment.setRegimen(inChemicalDrug.getRegimen());
+        theTreatment.setSexDistribution(sexDistribution);
+
+        // Append the ageunit onto the age at treatment variable
+        theTreatment.setAgeAtTreatment(inChemicalDrug.getAgeAtTreatment() + " " + inChemicalDrug.getAgeUnit());
+        theTreatment.setDosage(inChemicalDrug.getDosage() + " " + inChemicalDrug.getDoseUnit());
+        theTreatment.setAdministrativeRoute(inChemicalDrug.getAdministrativeRoute());
+
+        // Agent IS-A an EnvironmentalFactor
+        Agent theAgent = theTherapy.getAgent();
+        if (theAgent == null) {
+            theAgent = new Agent();
+            theTherapy.setAgent(theAgent);
+        }
+
+        theAgent.setName(inChemicalDrug.getName());
+
+        String theNSCNumber = inChemicalDrug.getNSCNumber().trim();
+        if (theNSCNumber != null && theNSCNumber.length() > 0) {
+            try {
+                theAgent.setNscNumber(Long.valueOf(theNSCNumber));
+            } catch (NumberFormatException e) {
+                log.error("Bad NSC number: " + theNSCNumber);
+            }
+        }
+        String theCasNumber = inChemicalDrug.getCASNumber().trim();
+        if (theCasNumber != null && theCasNumber.length() > 0) {
+            theAgent.setCasNumber(theCasNumber);
+        }
+        theAgent.setType("Chemical / Drug");
+
+        // TherapeuticExperiment property is false, tells us that this is an
+        // environmentalFactor
+        theTherapy.setTherapeuticExperiment(new Boolean(false));
+        theTherapy.setAgent(theAgent);
+        theTherapy.setTreatment(theTreatment);
     }
 }
--- src/gov/nih/nci/camod/webapp/action/ChemicalDrugAction.java
+++ src/gov/nih/nci/camod/webapp/action/ChemicalDrugAction.java
@@ -1,26 +1,14 @@
 package gov.nih.nci.camod.webapp.action;
 
-import java.util.List;
+import gov.nih.nci.camod.Constants;
+import gov.nih.nci.camod.domain.*;
+import gov.nih.nci.camod.service.*;
+import gov.nih.nci.camod.webapp.form.ChemicalDrugForm;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.ActionForward;
-import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionMessage;
-import org.apache.struts.action.ActionMessages;
-import gov.nih.nci.camod.Constants;
-import gov.nih.nci.camod.domain.Agent;
-import gov.nih.nci.camod.domain.AnimalModel;
-import gov.nih.nci.camod.domain.SexDistribution;
-import gov.nih.nci.camod.domain.Therapy;
-import gov.nih.nci.camod.domain.Treatment;
-import gov.nih.nci.camod.service.AgentManager;
-import gov.nih.nci.camod.service.AnimalModelManager;
-import gov.nih.nci.camod.service.SexDistributionManager;
-import gov.nih.nci.camod.service.TherapyManager;
-import gov.nih.nci.camod.service.TreatmentManager;
-import gov.nih.nci.camod.webapp.form.ChemicalDrugForm;
+
+import org.apache.struts.action.*;
 
 /**
  * ChemicalDrugAction Class
@@ -94,71 +82,30 @@ public class ChemicalDrugAction extends BaseAction {
                 + "\n\t otherName: " + chemicalDrugForm.getOtherName() + "\n\t type: " + chemicalDrugForm.getType()
                 + "\n\t regimen: " + chemicalDrugForm.getRegimen() + "\n\t dosage: " + chemicalDrugForm.getDosage()
                 + "\n\t doseUnit: " + chemicalDrugForm.getDoseUnit() + "\n\t ageAtTreatment: "
-                + chemicalDrugForm.getAgeAtTreatment() + "\n\t ageUnit: " + chemicalDrugForm.getAgeUnit());
+                + chemicalDrugForm.getAgeAtTreatment() + "\n\t ageUnit: " + chemicalDrugForm.getAgeUnit() + 
+                "\n\t administrativeRoute: " + chemicalDrugForm.getAdministrativeRoute());
 
         AnimalModelManager animalModelManager = (AnimalModelManager) getBean("animalModelManager");
-        SexDistributionManager sexDistributionManager = (SexDistributionManager) getBean("sexDistributionManager");
-        TreatmentManager treatmentManager = (TreatmentManager) getBean("treatmentManager");
-        AgentManager agentManager = (AgentManager) getBean("agentManager");
-        TherapyManager therapyManager = (TherapyManager) getBean("therapyManager");
-
         AnimalModel animalModel = animalModelManager.get(modelID);
 
-        // retrieve the list of all therapies from the current animalModel
-        List therapyList = animalModel.getTherapyCollection();
+        try {
 
-        Therapy ty = new Therapy();
-        int therapyNumber = 0;
+            animalModelManager.updateChemicalDrug(animalModel, chemicalDrugForm, aTherapyID);
 
-        // find the specific one we need
-        for (int i = 0; i < therapyList.size(); i++) {
-            ty = (Therapy) therapyList.get(i);
-            System.out.println(" searching ... id=" + ty.getId().toString() + " is it equal? " + aTherapyID);
-            if (ty.getId().toString().equals(aTherapyID)) {
-                therapyNumber = i;
-                System.out.println("found a match!");
-                break;
-            }
+            // Add a message to be displayed in submitOverview.jsp saying you've
+            // created a new model successfully
+            ActionMessages msg = new ActionMessages();
+            msg.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("chemicaldrug.edit.successful"));
+            saveErrors(request, msg);
+
+        } catch (Exception e) {
+
+            log.error("Unable to get add a chemical drug action: ", e);
+
+            ActionMessages theMsg = new ActionMessages();
+            theMsg.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("errors.admin.message"));
+            saveErrors(request, theMsg);
         }
-
-        // Set the treatment
-        Treatment ts = ty.getTreatment();
-
-        // Set the gender
-        SexDistribution sexDistribution = sexDistributionManager.getByType(chemicalDrugForm.getType());
-
-        // save the treatment
-        ts.setRegimen(chemicalDrugForm.getRegimen());
-        ts.setSexDistribution(sexDistribution);
-        // Append the ageunit onto the age at treatment variable
-        ts.setAgeAtTreatment(chemicalDrugForm.getAgeAtTreatment() + " " + chemicalDrugForm.getAgeUnit());
-        ts.setDosage(chemicalDrugForm.getDosage() + " " + chemicalDrugForm.getDoseUnit());
-        ts.setAdministrativeRoute(chemicalDrugForm.getAdministrativeRoute());
-        treatmentManager.save(ts);
-
-        // Agent IS-A an EnvironmentalFactor
-        Agent agent = ty.getAgent();
-        agent.setName(chemicalDrugForm.getName());
-        agent.setNscNumber(Long.valueOf(chemicalDrugForm.getNSCNumber().trim()));
-        agent.setCasNumber(chemicalDrugForm.getCASNumber());
-        agent.setType("Chemical / Drug");
-        agentManager.save(agent);
-
-        // TherapeuticExperiment property is false, tells us that this is an
-        // environmentalFactor
-        ty.setTherapeuticExperiment(new Boolean(false));
-        ty.setAgent(agent);
-        ty.setTreatment(ts);
-        therapyManager.save(ty);
-        therapyList.set(therapyNumber, ty);
-
-        animalModel.setTherapyCollection(therapyList);
-
-        // Add a message to be displayed in submitOverview.jsp saying you've
-        // created a new model successfully
-        ActionMessages msg = new ActionMessages();
-        msg.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("chemicaldrug.edit.successful"));
-        saveErrors(request, msg);
 
         return mapping.findForward("AnimalModelTreePopulateAction");
     }
@@ -186,7 +133,7 @@ public class ChemicalDrugAction extends BaseAction {
 
         ChemicalDrugForm chemicalDrugForm = (ChemicalDrugForm) form;
 
-        System.out.println("<GrowthFactorAction save> Adding... " + "\n\t name: " + chemicalDrugForm.getName()
+        System.out.println("<ChemicalDrugAction save> Adding... " + "\n\t name: " + chemicalDrugForm.getName()
                 + "\n\t otherName: " + chemicalDrugForm.getOtherName() + "\n\t type: " + chemicalDrugForm.getType()
                 + "\n\t regimen: " + chemicalDrugForm.getRegimen() + "\n\t dosage: " + chemicalDrugForm.getDosage()
                 + "\n\t doseUnit: " + chemicalDrugForm.getDoseUnit() + "\n\t ageAtTreatment: "
@@ -194,52 +141,28 @@ public class ChemicalDrugAction extends BaseAction {
                 + chemicalDrugForm.getCASNumber() + "\n\t ageUnit: " + chemicalDrugForm.getAgeUnit());
 
         AnimalModelManager animalModelManager = (AnimalModelManager) getBean("animalModelManager");
-        SexDistributionManager sexDistributionManager = (SexDistributionManager) getBean("sexDistributionManager");
-        TreatmentManager treatmentManager = (TreatmentManager) getBean("treatmentManager");
-        AgentManager agentManager = (AgentManager) getBean("agentManager");
-
+        
         AnimalModel animalModel = animalModelManager.get(modelID);
+        
+        try {
 
-        // Set the gender
-        SexDistribution sexDistribution = sexDistributionManager.getByType(chemicalDrugForm.getType());
+            animalModelManager.addChemicalDrug(animalModel, chemicalDrugForm);
 
-        // Set the treatment
-        Treatment ts = new Treatment();
-        ts.setRegimen(chemicalDrugForm.getRegimen());
-        ts.setSexDistribution(sexDistribution);
-        // Append the ageunit onto the age at treatment variable
-        ts.setAgeAtTreatment(chemicalDrugForm.getAgeAtTreatment() + " " + chemicalDrugForm.getAgeUnit());
-        ts.setDosage(chemicalDrugForm.getDosage() + " " + chemicalDrugForm.getDoseUnit());
-        ts.setAdministrativeRoute(chemicalDrugForm.getAdministrativeRoute());
-        treatmentManager.save(ts);
+            // Add a message to be displayed in submitOverview.jsp saying you've
+            // created a new model successfully
+            ActionMessages msg = new ActionMessages();
+            msg.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("chemicaldrug.edit.successful"));
+            saveErrors(request, msg);
 
-        // Agent IS-A an EnvironmentalFactor
-        Agent agent = new Agent();
-        agent.setName(chemicalDrugForm.getName());
-        agent.setNscNumber(Long.valueOf(chemicalDrugForm.getNSCNumber()));
-        agent.setCasNumber(chemicalDrugForm.getCASNumber());
-        agent.setType("Chemical / Drug");
-        agentManager.save(agent);
+        } catch (Exception e) {
 
-        // TherapeuticExperiment property is false, tells us that this is an
-        // environmentalFactor
-        Therapy ty = new Therapy();
-        ty.setTherapeuticExperiment(new Boolean(false));
-        ty.setAgent(agent);
-        ty.setTreatment(ts);
+            log.error("Unable to get add a chemical drug action: ", e);
 
-        // Add therapy to animalModel
-        animalModel.addTherapy(ty);
-
-        // Persist all changes to the db
-        animalModelManager.save(animalModel);
-
-        // Add a message to be displayed in submitOverview.jsp saying you've
-        // created a new model successfully
-        ActionMessages msg = new ActionMessages();
-        msg.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("chemicaldrug.edit.successful"));
-        saveErrors(request, msg);
-
+            ActionMessages theMsg = new ActionMessages();
+            theMsg.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("errors.admin.message"));
+            saveErrors(request, theMsg);
+        }
+        
         return mapping.findForward("AnimalModelTreePopulateAction");
     }
 
