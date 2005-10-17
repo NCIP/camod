4131aab5634f95f0667b1e757ad0d4caa842d453
georgeda <georgeda>
2005-10-17 18:19:28 +0000

    Added ability to sort
    SVN-Revision: 758

--- src/gov/nih/nci/camod/domain/AnimalModelSearchResult.java
+++ src/gov/nih/nci/camod/domain/AnimalModelSearchResult.java
@@ -1,9 +1,12 @@
 /**
  * @author dgeorge
  * 
- * $Id: AnimalModelSearchResult.java,v 1.4 2005-10-17 13:27:54 georgeda Exp $
+ * $Id: AnimalModelSearchResult.java,v 1.5 2005-10-17 18:19:28 georgeda Exp $
  * 
  * $Log: not supported by cvs2svn $
+ * Revision 1.4  2005/10/17 13:27:54  georgeda
+ * Updates
+ *
  * Revision 1.3  2005/10/12 18:18:48  georgeda
  * Small fix
  *
@@ -23,165 +26,169 @@ import java.util.List;
 /**
  * Used as wrapper around animal model for speedy display during paganation.
  */
-public class AnimalModelSearchResult {
+public class AnimalModelSearchResult implements Comparable {
 
-	private String myAnimalModelId;
+    private String myAnimalModelId;
 
-	private String myTumorSites = null;
+    private String myTumorSites = null;
 
-	private String myMetastatisSites = null;
+    private String myMetastatisSites = null;
 
-	private String mySpecies = null;
+    private String mySpecies = null;
 
-	private String myModelDescriptor = null;
+    private String myModelDescriptor = null;
 
-	private String mySubmitterName = null;
+    private String mySubmitterName = null;
 
-	private String mySubmittedDate = null;
+    private String mySubmittedDate = null;
 
-	private AnimalModel myAnimalModel = null;
+    private AnimalModel myAnimalModel = null;
 
-	/**
-	 * Create the wraper object
-	 * 
-	 * @param inAnimalModel
-	 *            the animal model we will be wrapping. Saves only the id.
-	 */
-	public AnimalModelSearchResult(AnimalModel inAnimalModel) {
-		myAnimalModelId = inAnimalModel.getId().toString();
-	}
+    /**
+     * Create the wraper object
+     * 
+     * @param inAnimalModel
+     *            the animal model we will be wrapping. Saves only the id.
+     */
+    public AnimalModelSearchResult(AnimalModel inAnimalModel) {
+        myAnimalModelId = inAnimalModel.getId().toString();
+        myModelDescriptor = inAnimalModel.getModelDescriptor();
+    }
 
-	/**
-	 * Return the id for the associated model
-	 * 
-	 * @return the id for the model
-	 */
-	public String getId() {
+    /**
+     * Return the id for the associated model
+     * 
+     * @return the id for the model
+     */
+    public String getId() {
 
-		return myAnimalModelId;
-	}
+        return myAnimalModelId;
+    }
 
-	/**
-	 * Return the model descriptor. It will fetch the animal model from the DB
-	 * if it hasn't already happened.
-	 * 
-	 * @return the model descriptor for the associated model
-	 * 
-	 * @throws Exception
-	 */
-	public String getModelDescriptor() throws Exception {
+    /**
+     * Return the model descriptor. It will fetch the animal model from the DB
+     * if it hasn't already happened.
+     * 
+     * @return the model descriptor for the associated model
+     * 
+     * @throws Exception
+     */
+    public String getModelDescriptor() throws Exception {
+        return myModelDescriptor;
+    }
 
-		if (myModelDescriptor == null) {
-			fetchAnimalModel();
-			myModelDescriptor = myAnimalModel.getModelDescriptor();
-		}
-		return myModelDescriptor;
-	}
+    /**
+     * Return the species. It will fetch the animal model from the DB if it
+     * hasn't already happened.
+     * 
+     * @return the species for the associated model
+     * @throws Exception
+     */
+    public String getSpecies() throws Exception {
+        if (mySpecies == null) {
+            fetchAnimalModel();
+            mySpecies = myAnimalModel.getSpecies().getScientificName();
+        }
+        return mySpecies;
+    }
 
-	/**
-	 * Return the species. It will fetch the animal model from the DB if it
-	 * hasn't already happened.
-	 * 
-	 * @return the species for the associated model
-	 * @throws Exception
-	 */
-	public String getSpecies() throws Exception {
-		if (mySpecies == null) {
-			fetchAnimalModel();
-			mySpecies = myAnimalModel.getSpecies().getScientificName();
-		}
-		return mySpecies;
-	}
+    /**
+     * Return the list of tumor sites. It will fetch the animal model from the
+     * DB if it hasn't already happened.
+     * 
+     * @return the list of tumor sites for the associated model
+     * @throws Exception
+     */
+    public String getTumorSites() throws Exception {
 
-	/**
-	 * Return the list of tumor sites. It will fetch the animal model from the
-	 * DB if it hasn't already happened.
-	 * 
-	 * @return the list of tumor sites for the associated model
-	 * @throws Exception
-	 */
-	public String getTumorSites() throws Exception {
+        if (myTumorSites == null) {
+            fetchAnimalModel();
 
-		if (myTumorSites == null) {
-			fetchAnimalModel();
+            myTumorSites = "";
+            List theOrgans = myAnimalModel.getDistinctOrgansFromHistopathologyCollection();
 
-			myTumorSites = "";
-			List theOrgans = myAnimalModel.getDistinctOrgansFromHistopathologyCollection();
+            for (int i = 0, j = theOrgans.size(); i < j; i++) {
+                String theOrgan = (String) theOrgans.get(i);
+                myTumorSites += "<b>" + theOrgan + "</b><br>";
+            }
+        }
 
-			for (int i = 0, j = theOrgans.size(); i < j; i++) {
-				String theOrgan = (String) theOrgans.get(i);
-				myTumorSites += "<b>" + theOrgan + "</b><br>";
-			}
-		}
+        return myTumorSites;
+    }
 
-		return myTumorSites;
-	}
+    /**
+     * Return the list of metastatis sites. It will fetch the animal model from
+     * the DB if it hasn't already happened.
+     * 
+     * @return the list of metastatis sites for the associated model
+     * @throws Exception
+     */
+    public String getMetastatisSites() throws Exception {
 
-	/**
-	 * Return the list of metastatis sites. It will fetch the animal model from
-	 * the DB if it hasn't already happened.
-	 * 
-	 * @return the list of metastatis sites for the associated model
-	 * @throws Exception
-	 */
-	public String getMetastatisSites() throws Exception {
+        if (myMetastatisSites == null) {
+            fetchAnimalModel();
 
-		if (myMetastatisSites == null) {
-			fetchAnimalModel();
+            myMetastatisSites = "";
+            List theOrgans = myAnimalModel.getDistinctMetastatisOrgansFromHistopathologyCollection();
 
-			myMetastatisSites = "";
-			List theOrgans = myAnimalModel.getDistinctMetastatisOrgansFromHistopathologyCollection();
+            for (int i = 0, j = theOrgans.size(); i < j; i++) {
+                String theOrgan = (String) theOrgans.get(i);
+                myMetastatisSites += theOrgan + " (Metastatis)<br>";
+            }
+        }
+        return myMetastatisSites;
+    }
 
-			for (int i = 0, j = theOrgans.size(); i < j; i++) {
-				String theOrgan = (String) theOrgans.get(i);
-				myMetastatisSites += theOrgan + " (Metastatis)<br>";
-			}
-		}
-		return myMetastatisSites;
-	}
+    /**
+     * Gets the display name of the submitter in an html linked format
+     * 
+     * @return the display name of the submitter
+     * @throws Exception
+     */
+    public String getSubmitterName() throws Exception {
 
-	/**
-	 * Gets the display name of the submitter in an html linked format
-	 * 
-	 * @return the display name of the submitter
-	 * @throws Exception
-	 */
-	public String getSubmitterName() throws Exception {
+        if (mySubmitterName == null) {
+            fetchAnimalModel();
 
-		if (mySubmitterName == null) {
-			fetchAnimalModel();
+            String theEmailAddress = myAnimalModel.getSubmitter().emailAddress();
 
-			String theEmailAddress = myAnimalModel.getSubmitter().emailAddress();
+            if (theEmailAddress.length() > 0) {
+                mySubmitterName = "<a href=\"mailto:" + theEmailAddress + "\"/>"
+                        + myAnimalModel.getSubmitter().displayName();
+            } else {
+                mySubmitterName = myAnimalModel.getSubmitter().displayName();
+            }
+        }
+        return mySubmitterName;
+    }
 
-			if (theEmailAddress.length() > 0) {
-				mySubmitterName = "<a href=\"mailto:" + theEmailAddress +  "\"/>" + myAnimalModel.getSubmitter().displayName();
-			} else {
-				mySubmitterName = myAnimalModel.getSubmitter().displayName();
-			}
-		}
-		return mySubmitterName;
-	}
+    /**
+     * Gets the date for which the model was submitted
+     * 
+     * @return the date the model was submitted
+     * @throws Exception
+     */
+    public String getSubmittedDate() throws Exception {
 
-	/**
-	 * Gets the date for which the model was submitted
-	 * 
-	 * @return the date the model was submitted
-	 * @throws Exception
-	 */
-	public String getSubmittedDate() throws Exception {
+        if (mySubmittedDate == null) {
+            fetchAnimalModel();
 
-		if (mySubmittedDate == null) {
-			fetchAnimalModel();
+            mySubmittedDate = myAnimalModel.getAvailability().getEnteredDate().toString();
+        }
+        return mySubmittedDate;
+    }
 
-			mySubmittedDate = myAnimalModel.getAvailability().getEnteredDate().toString();
-		}
-		return mySubmittedDate;
-	}
+    public int compareTo(Object inObject) {
 
-	// Fetch the animal model from the DB
-	private void fetchAnimalModel() throws Exception {
-		if (myAnimalModel == null) {
-			myAnimalModel = AnimalModelManagerSingleton.instance().get(myAnimalModelId);
-		}
-	}
+        AnimalModelSearchResult theResult = (AnimalModelSearchResult) inObject;
+
+        return this.myModelDescriptor.compareTo(theResult.myModelDescriptor);
+    }
+
+    // Fetch the animal model from the DB
+    private void fetchAnimalModel() throws Exception {
+        if (myAnimalModel == null) {
+            myAnimalModel = AnimalModelManagerSingleton.instance().get(myAnimalModelId);
+        }
+    }
 }
