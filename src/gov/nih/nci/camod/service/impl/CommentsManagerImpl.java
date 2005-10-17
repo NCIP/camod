ec59c5e2cbe95c97fa4bffc0321f323b614e0074
georgeda <georgeda>
2005-10-17 13:26:50 +0000

    Updates for curation
    SVN-Revision: 736

--- src/gov/nih/nci/camod/service/impl/CommentsManagerImpl.java
+++ src/gov/nih/nci/camod/service/impl/CommentsManagerImpl.java
@@ -1,9 +1,12 @@
 /**
  * @author dgeorge
  * 
- * $Id: CommentsManagerImpl.java,v 1.2 2005-10-11 18:13:01 georgeda Exp $
+ * $Id: CommentsManagerImpl.java,v 1.3 2005-10-17 13:26:50 georgeda Exp $
  * 
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2005/10/11 18:13:01  georgeda
+ * More comment changes
+ *
  * Revision 1.1  2005/10/10 14:07:31  georgeda
  * Initial revision
  *
@@ -12,11 +15,16 @@
 
 package gov.nih.nci.camod.service.impl;
 
-import gov.nih.nci.camod.domain.*;
+import gov.nih.nci.camod.domain.AnimalModel;
+import gov.nih.nci.camod.domain.Availability;
+import gov.nih.nci.camod.domain.Comments;
+import gov.nih.nci.camod.domain.ModelSection;
+import gov.nih.nci.camod.domain.Person;
 import gov.nih.nci.camod.service.CommentsManager;
 import gov.nih.nci.camod.webapp.form.CommentsData;
 import gov.nih.nci.common.persistence.Search;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -24,122 +32,128 @@ import java.util.List;
  */
 public class CommentsManagerImpl extends BaseManager implements CommentsManager {
 
-    /**
-     * Get all of the comments in the DB
-     * 
-     * @return the list of all animal models
-     * 
-     * @exception throws
-     *                an Exception if an error occurred
-     */
-    public Comments create(CommentsData inCommentsData) throws Exception {
-        log.trace("In CommentsManagerImpl.create");
+	/**
+	 * Get all of the comments in the DB
+	 * 
+	 * @return the list of all animal models
+	 * 
+	 * @exception throws
+	 *                an Exception if an error occurred
+	 */
+	public Comments create(CommentsData inCommentsData) throws Exception {
+		log.trace("In CommentsManagerImpl.create");
 
-        Comments theComments = new Comments();
+		Comments theComments = new Comments();
 
-        theComments.setRemark(inCommentsData.getRemark());
+		theComments.setRemark(inCommentsData.getRemark());
 
-        // Set the model section
-        ModelSection theQBEModelSection = new ModelSection();
-        theQBEModelSection.setName(inCommentsData.getSectionName());
+		// Set the model section
+		ModelSection theQBEModelSection = new ModelSection();
+		theQBEModelSection.setName(inCommentsData.getSectionName());
 
-        List theList = Search.query(theQBEModelSection);
+		List theList = Search.query(theQBEModelSection);
 
-        if (theList.size() > 0) {
-            ModelSection theModelSection = (ModelSection) theList.get(0);
-            theComments.setModelSection(theModelSection);
-        } else {
-            throw new IllegalArgumentException("Unknown model section name: " + inCommentsData.getSectionName());
-        }
+		if (theList.size() > 0) {
+			ModelSection theModelSection = (ModelSection) theList.get(0);
+			theComments.setModelSection(theModelSection);
+		} else {
+			throw new IllegalArgumentException("Unknown model section name: " + inCommentsData.getSectionName());
+		}
 
-        // Add the submitter
-        Person theSubmitter = PersonManagerSingleton.instance().getByUsername(inCommentsData.getSubmitter());
-        if (theSubmitter == null) {
-            throw new IllegalArgumentException("Invalid submitter: " + inCommentsData.getSubmitter());
-        }
-        theComments.setSubmitter(theSubmitter);
+		// Add the submitter
+		Person theSubmitter = PersonManagerSingleton.instance().getByUsername(inCommentsData.getSubmitter());
+		if (theSubmitter == null) {
+			throw new IllegalArgumentException("Invalid submitter: " + inCommentsData.getSubmitter());
+		}
+		theComments.setSubmitter(theSubmitter);
 
-        // Set the cancer model
-        AnimalModel theAnimalModel = AnimalModelManagerSingleton.instance().get(inCommentsData.getModelId());
+		// Set the cancer model
+		AnimalModel theAnimalModel = AnimalModelManagerSingleton.instance().get(inCommentsData.getModelId());
 
-        if (theAnimalModel == null) {
-            throw new IllegalArgumentException("Invalid model id: " + inCommentsData.getModelId());
-        }
+		if (theAnimalModel == null) {
+			throw new IllegalArgumentException("Invalid model id: " + inCommentsData.getModelId());
+		}
+		theComments.setCancerModel(theAnimalModel);
 
-        theComments.setCancerModel(theAnimalModel);
+		// Create an availability
+		Availability theAvailability = new Availability();
+		theAvailability.setEnteredDate(new Date());
+		theAvailability.setReleaseDate(new Date());
 
-        // Set the state
-        theComments.setState(inCommentsData.getState());
-        
-        return theComments;
-    }
+		theComments.setAvailability(theAvailability);
 
-    /**
-     * Get all of the comments in the DB
-     * 
-     * @return the list of all animal models
-     * 
-     * @exception throws
-     *                an Exception if an error occurred
-     */
-    public List getAll() throws Exception {
-        log.trace("In CommentsManagerImpl.getAll");
-        return super.getAll(AnimalModel.class);
-    }
+		// Set the state
+		theComments.setState(inCommentsData.getState());
 
-    /**
-     * Get all of the comments of a specific state
-     * 
-     * @param inState
-     *            the state to query for
-     * 
-     * @return the list of comments
-     * 
-     * @exception Exception
-     *                if an error occurred
-     */
-    public List getAllByStateForPerson(String inState, Person inPerson) throws Exception {
+		return theComments;
+	}
 
-        log.trace("In CommentsManagerImpl.getAllByStateForPerson");
+	/**
+	 * Get all of the comments in the DB
+	 * 
+	 * @return the list of all animal models
+	 * 
+	 * @exception throws
+	 *                an Exception if an error occurred
+	 */
+	public List getAll() throws Exception {
+		log.trace("In CommentsManagerImpl.getAll");
+		return super.getAll(AnimalModel.class);
+	}
 
-        return QueryManagerSingleton.instance().getCommentsByStateForPerson(inState, inPerson);
-    }
+	/**
+	 * Get all of the comments of a specific state
+	 * 
+	 * @param inState
+	 *            the state to query for
+	 * 
+	 * @return the list of comments
+	 * 
+	 * @exception Exception
+	 *                if an error occurred
+	 */
+	public List getAllByStateForPerson(String inState, Person inPerson) throws Exception {
 
-    /**
-     * Get all of the comments for a specifed section by person
-     * 
-     * @param inSection
-     *            the section the comment is associated with
-     * @param inPerson
-     *            the person the comment is associated with (allows users to
-     *            view unrelease comments if they are their own)
-     * 
-     * @return the list of comments
-     * 
-     * @exception Exception
-     *                if an error occurred
-     */
-    public List getAllBySection(String inState, Person inPerson, AnimalModel inModel) throws Exception {
+		log.trace("In CommentsManagerImpl.getAllByStateForPerson");
 
-        log.trace("In CommentsManagerImpl.getAllBySectionForPerson");
+		return QueryManagerSingleton.instance().getCommentsByStateForPerson(inState, inPerson);
+	}
 
-        return QueryManagerSingleton.instance().getCommentsBySection(inState, inPerson, inModel);
-    }
+	/**
+	 * Get all of the comments for a specifed section by person
+	 * 
+	 * @param inSection
+	 *            the section the comment is associated with
+	 * @param inPerson
+	 *            the person the comment is associated with (allows users to
+	 *            view unrelease comments if they are their own)
+	 * 
+	 * @return the list of comments
+	 * 
+	 * @exception Exception
+	 *                if an error occurred
+	 */
+	public List getAllBySection(String inState, Person inPerson, AnimalModel inModel) throws Exception {
 
-    /**
-     * Get a specific comment
-     * 
-     * @param id
-     *            The unique id for the comments
-     * 
-     * @return the comment if found, null otherwise
-     * @throws Exception
-     * 
-     * @exception Exception
-     *                if an error occurred
-     */
-    public Comments get(String id) throws Exception {
-        log.trace("In CommentsManagerImpl.get");
-        return (Comments) super.get(id, Comments.class);
-    }
+		log.trace("In CommentsManagerImpl.getAllBySectionForPerson");
+
+		return QueryManagerSingleton.instance().getCommentsBySection(inState, inPerson, inModel);
+	}
+
+	/**
+	 * Get a specific comment
+	 * 
+	 * @param id
+	 *            The unique id for the comments
+	 * 
+	 * @return the comment if found, null otherwise
+	 * @throws Exception
+	 * 
+	 * @exception Exception
+	 *                if an error occurred
+	 */
+	public Comments get(String id) throws Exception {
+		log.trace("In CommentsManagerImpl.get");
+		return (Comments) super.get(id, Comments.class);
+	}
 }
