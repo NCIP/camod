06676dff681754040c296c4d6fb750835a9e01b0
Duane Steward <stewardd>
2005-10-18 19:38:19 +0000

    Extended previous overloading to support variables within textblocks
    SVN-Revision: 780

--- src/gov/nih/nci/camod/util/MailUtil.java
+++ src/gov/nih/nci/camod/util/MailUtil.java
@@ -1,6 +1,7 @@
 package gov.nih.nci.camod.util;
 
 import java.util.*;
+import java.io.StringWriter;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
@@ -8,6 +9,10 @@ import javax.mail.internet.MimeMessage;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.tools.generic.log.CommonsLogLogSystem;
 
 /**
  * Static helper class for sending e-mail.  Propeteries are defined in "mail.properties"
@@ -15,7 +20,7 @@ import org.apache.commons.logging.LogFactory;
  * @author georgeda
  *
  */
- public class MailUtil {
+public class MailUtil {
 
     static private final Log log = LogFactory.getLog(MailUtil.class);
 
@@ -32,11 +37,14 @@ import org.apache.commons.logging.LogFactory;
      * @param inFrom - who the message is from
      * 
      */
-    static public void sendMail(String inRecipients[], String inSubject, String inMessage, String inFrom)
+    static public void sendMail(String inRecipients[],
+                                String inSubject,
+                                String inMessage,
+                                String inFrom)
             throws MessagingException {
 
         log.trace("Entering sendMail");
-        
+
         try {
 
             // Convert the bundle to a properties file.  Is there a better way to do this?
@@ -82,13 +90,13 @@ import org.apache.commons.logging.LogFactory;
 
             log.debug("Sending email to: " + inRecipients);
             log.debug("email subject: " + inSubject);
-           
+
             Transport.send(theEmailMessage);
 
         } catch (Exception e) {
             log.error("Unable to send e-mail", e);
         }
-        
+
         log.trace("Exiting sendMail");
     }
 
@@ -100,7 +108,12 @@ import org.apache.commons.logging.LogFactory;
      * @param messageStds - an array of Strings to be used as keys to create the message to send
      *
      */
-    static public void sendMail(String inRecipients[], String inSubject, String inMessage, String inFrom, String[] messageStds)
+    static public void sendMail(String inRecipients[],
+                                String inSubject,
+                                String inMessage,
+                                String inFrom,
+                                String[] messageStds,
+                                TreeMap valuesForVariables)
             throws MessagingException {
         log.trace("Entering sendMail(String, String, String, String, String[])");
 
@@ -146,9 +159,9 @@ import org.apache.commons.logging.LogFactory;
             // Setting the Subject and Content Type
             theEmailMessage.setSubject(inSubject);
             if (null == inMessage || "".equals(inMessage)) {
-                theEmailMessage.setContent(constructMessageFromMacros(messageStds), "text/plain");
+                theEmailMessage.setContent(buildContent(valuesForVariables,constructTemplateFromMacros(messageStds)), "text/plain");
             } else {
-                theEmailMessage.setContent(constructMessageFromMacros(messageStds)
+                theEmailMessage.setContent(constructTemplateFromMacros(messageStds)
                         + lineseparator + lineseparator + inMessage , "text/plain");
             }
 
@@ -165,17 +178,18 @@ import org.apache.commons.logging.LogFactory;
     }
 
     /**
-     * Private method to construct the message body from an array of keys
+     * Private method to construct the message body template from an array of keys
      *
      * @param macros is the list of keys for expansion
      * @return String with macros expanded to use as the body of an e-mail message
      */
-    private static String constructMessageFromMacros(String[] macros) {
+    private static String constructTemplateFromMacros(String[] macros) {
         StringBuffer sb = new StringBuffer();
         try {
+            /* exception caught here to ensure graceful failure */
             if (null==macroBundle) loadMacroBundle();
         } catch (MissingResourceException e) {
-            log.error("Unable to load Macro ResourceBundle from within constructMessageFromMacros(macros)", e);
+            log.error("Unable to load Macro ResourceBundle from within constructTemplateFromMacros(macros)", e);
             return "A problem was encountered in generating automated e-mail. Please contact System Administrator";
         }
         for (int i = 0; i < macros.length; i++) {
@@ -196,16 +210,93 @@ import org.apache.commons.logging.LogFactory;
         log.trace("done loading Macro ResourceBundle");
     }
 
+    private static String buildContentWTemplateFile(TreeMap dict, String templateFile) {
+        log.trace("starting up in buildContentWTemplateFile");
+        try {
+            VelocityEngine ve = new VelocityEngine();
+            ve.setProperty(Velocity.RUNTIME_LOG_LOGSYSTEM_CLASS, CommonsLogLogSystem.class.getName());
+            /* init runtime engine */
+            ve.init();
+            log.trace("after Velocity init");
+            /* make a Context and put data into it */
+            VelocityContext context = new VelocityContext();
+            Iterator iter = dict.keySet().iterator();
+            while (iter.hasNext()) {
+                String key = (String) iter.next();
+                context.put(key,dict.get(key));
+            }
+            /* render template */
+            StringWriter sw = new StringWriter();
+            ve.mergeTemplate(templateFile,"ISO-8859-1",context, sw);
+            return sw.toString();
+        } catch (Exception e) {
+            log.error("Unable to perform substitutions from within buildContentWTemplateFile(...)", e);
+            return "A problem was encountered in generating automated e-mail. Please contact System Administrator";
+        }
+    }
+
+    /**
+     * Method to construct the content for e-mail messages based on a given template.
+     * Variables are supported--i.e., variables within the template will have values substituted in the returned
+     * output. Velocity is employed for this so the template must adhere to the Velocity Template Language.
+     * Variables provided in the dictionary that are not used in the template are simply ignored. It is assumed
+     * that the template will gracefully handle any variables for which there are no values found in the dictionary.
+     *
+     * @param myDictionary of key/value pairs to prescribe values for variables found in the template
+     * @param mytemplate to be used to generate the text output
+     * @return String output
+     */
+    private static String buildContent(TreeMap myDictionary, String mytemplate) {
+        log.trace("starting up in buildContent");
+        try {
+            VelocityEngine ve = new VelocityEngine();
+            ve.setProperty(Velocity.RUNTIME_LOG_LOGSYSTEM_CLASS, CommonsLogLogSystem.class.getName());
+            /* init runtime engine */
+            ve.init();
+            // the previous line throws java.lang.InstantiationException: org.apache.commons.logging.impl.Jdk14Logger
+            // if ve is asked to set Velocity.RUNTIME_LOG_LOGSYSTEM_CLASS to log.getClass().getName()
+            // and then the "runtime.log.logsystem.log4j.category" property is set to "global"
+            log.trace("after Velocity init");
+            /* make a Context and put data into it */
+            VelocityContext context = new VelocityContext();
+            Iterator iter = myDictionary.keySet().iterator();
+            while (iter.hasNext()) {
+                String key = (String) iter.next();
+                context.put(key,myDictionary.get(key));
+            }
+            /* render template */
+            StringWriter sw = new StringWriter();
+            ve.evaluate(context, sw,"MailUtil.buildContent(...)", mytemplate);
+            return sw.toString();
+        } catch (Exception e) {
+            log.error("Unable to perform substitutions from within MailUtil.buildContent(...)", e);
+            return "A problem was encountered in generating automated e-mail. Please contact System Administrator";
+        }
+    }
+
     static public void main(String[] inArgs) {
 
         String[] theRecipients = new String[1];
         theRecipients[0] = new String("georgeda@mail.nih.gov");
 
         String[] theMessageKeys = {"approve","assign_editor","assign_screener", "need_more_info", "reject","complete"};
-
+        String[] macrosUsed = {"defaultbody","alternative1","alternative2"};
+        TreeMap valuesForVariables = new TreeMap();
+        valuesForVariables.put("name","Fibber McGhee");
+        valuesForVariables.put("submitter","Fibber McGhee");
+        valuesForVariables.put("profession","bustin gut");
+        valuesForVariables.put("alternative","particularly relevant");
+        valuesForVariables.put("possibility","personality");
+        System.out.println(buildContentWTemplateFile(valuesForVariables,"email0.template"));
+        System.out.println(lineseparator);
+        System.out.println(buildContent(valuesForVariables, constructTemplateFromMacros(macrosUsed)));
         try {
             sendMail(theRecipients, "Testing", "Hello there!", "georgeda@mail.nih.gov");
-            sendMail(theRecipients, "Testing 2", "Additional custom note goes here.", "georgeda@mail.nih.gov", theMessageKeys);
+            sendMail(theRecipients, "Testing 2",
+                    "Additional custom note goes here.",
+                    "georgeda@mail.nih.gov",
+                    theMessageKeys,
+                    valuesForVariables);
         } catch (Exception e) {
             System.out.println("Caught exception" + e);
             e.printStackTrace();
