7527d38bf9930ab122e524417c066cba7f1c8d04
georgeda <georgeda>
2005-10-26 20:54:51 +0000

    Put transaction around remove
    SVN-Revision: 939

--- src/gov/nih/nci/camod/service/impl/BaseManager.java
+++ src/gov/nih/nci/camod/service/impl/BaseManager.java
@@ -1,9 +1,12 @@
 /**
  * @author dgeorge
  * 
- * $Id: BaseManager.java,v 1.3 2005-09-29 18:31:14 georgeda Exp $
+ * $Id: BaseManager.java,v 1.4 2005-10-26 20:54:51 georgeda Exp $
  * 
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2005/09/29 18:31:14  georgeda
+ * Changed visibility of base functions to protected
+ *
  * Revision 1.2  2005/09/26 14:02:38  georgeda
  * Added common code
  *
@@ -23,11 +26,12 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- * Base class for managers.  Provides the generic Object based calls.
+ * Base class for managers. Provides the generic Object based calls.
  */
 public class BaseManager implements Manager {
+
     protected final Log log = LogFactory.getLog(getClass());
-    
+
     /**
      * Get all of the objects models in the DB
      * 
@@ -104,7 +108,7 @@ public class BaseManager implements Manager {
             // Begin an transaction
             HibernateUtil.beginTransaction();
 
-            //Save the object
+            // Save the object
             log.debug("Saving object");
             Persist.save(inObject);
 
@@ -123,8 +127,8 @@ public class BaseManager implements Manager {
     }
 
     /**
-     * Remove an object from the system. Should remove all associated
-     * data as well
+     * Remove an object from the system. Should remove all associated data as
+     * well
      * 
      * @param inId
      *            The unique id of the object to delete
@@ -137,12 +141,21 @@ public class BaseManager implements Manager {
         log.trace("Entering BaseManager.remove");
 
         try {
+            // Begin an transaction
+            HibernateUtil.beginTransaction();
+
             log.debug("Removing object: " + inId);
             Persist.deleteById(inClass, new Long(inId));
+
+            // Commit all changes or none
+            HibernateUtil.commitTransaction();
+
         } catch (PersistenceException pe) {
+            HibernateUtil.rollbackTransaction();
             log.error("Unable to delete object: ", pe);
             throw pe;
         } catch (Exception e) {
+            HibernateUtil.rollbackTransaction();
             log.error("Unable to delete object: ", e);
             throw e;
         }
