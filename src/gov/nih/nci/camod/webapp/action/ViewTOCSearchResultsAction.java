7b47ee2a3886809dd470315a232d168a9fbffa8a
georgeda <georgeda>
2005-12-12 17:11:55 +0000

    Defect #266, allow external linkage of TOC queries and added head and neck TOC entry
    SVN-Revision: 1535

--- src/gov/nih/nci/camod/webapp/action/ViewTOCSearchResultsAction.java
+++ src/gov/nih/nci/camod/webapp/action/ViewTOCSearchResultsAction.java
@@ -1,8 +1,7 @@
 package gov.nih.nci.camod.webapp.action;
 
 import gov.nih.nci.camod.Constants;
-import gov.nih.nci.camod.service.impl.TOCQuery;
-import gov.nih.nci.camod.service.impl.TOCQueryGroup;
+import gov.nih.nci.camod.service.impl.*;
 
 import java.util.List;
 
@@ -18,12 +17,26 @@ public class ViewTOCSearchResultsAction extends BaseAction {
 
         log.trace("In ViewTOCSearchResultsAction.populate");
 
-        String theKey = (String) request.getParameter(Constants.Parameters.TOCQUERYKEY);
-
         String theForward = "next";
 
-        List theGroupList = (List) request.getSession().getAttribute(Constants.TOCSearch.TOC_QUERY_RESULTS);
         try {
+
+            String theKey = (String) request.getParameter(Constants.Parameters.TOCQUERYKEY);
+
+            // Handle external linkage
+            if (request.getSession().getAttribute(Constants.TOCSearch.TOC_QUERY_RESULTS) == null) {
+
+                // Get the TOC manager workflow
+                TOCManager theTOCManager = new TOCManager(getServlet().getServletContext().getRealPath("/")
+                        + Constants.TOCSearch.TOC_QUERY_FILE);
+
+                List theResults = theTOCManager.process();
+
+                request.getSession().setAttribute(Constants.TOCSearch.TOC_QUERY_RESULTS, theResults);
+            }
+
+            List theGroupList = (List) request.getSession().getAttribute(Constants.TOCSearch.TOC_QUERY_RESULTS);
+
             for (int i = 0; i < theGroupList.size(); i++) {
                 TOCQueryGroup theQueryGroup = (TOCQueryGroup) theGroupList.get(i);
                 List theQueryList = theQueryGroup.getQueries();
@@ -36,6 +49,7 @@ public class ViewTOCSearchResultsAction extends BaseAction {
                     }
                 }
             }
+            
         } catch (Exception e) {
 
             theForward = "failure";
