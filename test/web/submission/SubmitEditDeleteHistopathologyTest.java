94d23b455c0c122a22fd009d2398e26ec4172c78
Sima Pandya <pandyas@mail.nih.gov>
2005-12-13 21:56:32 +0000

    JUnit test case for SubmitDeleteEditHistopathology - depends on other changes in main path that will NOT be uploaded until we go to production
    SVN-Revision: 1567

--- test/web/submission/SubmitEditDeleteHistopathologyTest.java
+++ test/web/submission/SubmitEditDeleteHistopathologyTest.java
@@ -1,9 +1,12 @@
 /**
  * @author pandyas
  * 
- * $Id: SubmitEditDeleteHistopathologyTest.java,v 1.1 2005-12-13 20:21:16 pandyas Exp $
+ * $Id: SubmitEditDeleteHistopathologyTest.java,v 1.2 2005-12-13 21:56:32 pandyas Exp $
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2005/12/13 20:21:16  pandyas
+ * initial version does not work
+ *
  * 
  */
 package web.submission;
@@ -48,46 +51,74 @@ public class SubmitEditDeleteHistopathologyTest extends BaseModelNeededTest {
     public void testHistopathology() throws Exception {
     	navigateToModelForEditing(myModelName);
         
-        // Find Histopathology link to Submit
+        /*  Find Histopathology link to Submit */
         WebLink theLink = myWebConversation.getCurrentPage()
                 .getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "Enter Histopathology");
         WebResponse theCurrentPage = theLink.click(); 
         assertCurrentPageContains("Tumor Incidence");
         WebForm theForm = theCurrentPage.getFormWithName("histopathologyForm");
-   
-        //theForm.getScriptableObject().setParameterValue( "organ", "Heart_MMHCC" );
         theForm.getScriptableObject().setParameterValue( "organTissueCode", "C22498" );
-        //theForm.getScriptableObject().setParameterValue( "organTissueName", "Heart" );
-        //theForm.getScriptableObject().setParameterValue( "tumorClassification", "Myeloid_Dysplasia_of_Mouse" );
-        //theForm.getScriptableObject().setParameterValue( "diagnosisName", "Myeloid_Dysplasia" ); 
-        theForm.setParameter( "diagnosisCode", "C21898");
-        //.getScriptableObject().setParameterValue( "diagnosisCode", "C21898" );        
-
-        //addPresetParameter("diagnosisCode", "C21898");
-        //theForm.setParameter("diagnosisCode", "C21898");        
-        //theForm.setParameter("diagnosisName", "Myeloid Dysplasia");
-
-
+        theForm.getScriptableObject().setParameterValue( "diagnosisCode", "C21898" );        
         theCurrentPage = theForm.submit();
         assertCurrentPageContains("You have successfully added a Histopathology to this model!");
         
-        /* Find Histopathology link to Edit 
+        /* Find Histopathology link to Edit */ 
         theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "Heart");        
         assertNotNull("Unable to find link to edit the Histopathology", theLink);        
         theCurrentPage = theLink.click();        
         assertCurrentPageContains("Tumor Incidence over Lifetime ");
         theForm = theCurrentPage.getFormWithName("histopathologyForm");
-        theForm.getScriptableObject().setParameterValue( "organ", "Heart_MMHCC" );
         theForm.getScriptableObject().setParameterValue( "organTissueCode", "C22498" );
-        theForm.getScriptableObject().setParameterValue( "organTissueName", "Heart" );
-        theForm.getScriptableObject().setParameterValue( "tumorClassification", "Cardiovascular System Neoplasms" );
         theForm.getScriptableObject().setParameterValue( "diagnosisCode", "C22918" );
-        theForm.getScriptableObject().setParameterValue( "diagnosisName", "Cardiovascular System Neoplasms" ); 
         theForm.setParameter("comments", "Unit Test Case");          
         theCurrentPage = theForm.submit();
         assertCurrentPageContains("You have successfully edited a Histopathology.");      
-        */
-        /* Find Histopathology link to Delete 
+        
+        /* Find Assoc Metastasis link to Submit */
+        theLink = myWebConversation.getCurrentPage()
+                .getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "Enter Assoc Metastasis");
+        theCurrentPage = theLink.click(); 
+        assertCurrentPageContains("Age of Metastasis");
+        theForm = theCurrentPage.getFormWithName("associatedMetastasisForm");  
+        theForm.getScriptableObject().setParameterValue( "organTissueCode", "C22517" );
+        theForm.getScriptableObject().setParameterValue( "diagnosisCode", "C21898" );
+        theCurrentPage = theForm.submit();
+        assertCurrentPageContains("You have successfully added an Associated Metastasis to this model!");
+        
+        /* Find Assoc Metastasis link to Edit */
+        theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "Liver - ");        
+        assertNotNull("Unable to find link to edit the Assoc Metastasis", theLink);        
+        theCurrentPage = theLink.click();        
+        assertCurrentPageContains("Age of Metastasis");
+        theForm = theCurrentPage.getFormWithName("associatedMetastasisForm");
+        theForm.getScriptableObject().setParameterValue( "organTissueCode", "C22517" );
+        theForm.getScriptableObject().setParameterValue( "diagnosisCode", "C21898" );
+        theForm.setParameter("comments", "Unit Test Case");          
+        theCurrentPage = theForm.submit();
+        assertCurrentPageContains("You have successfully edited an Associated Metastasis.");      
+        
+        /* Find Clinical Marker link to Submit */
+        theLink = myWebConversation.getCurrentPage()
+                .getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "Enter Clinical Marker");
+        theCurrentPage = theLink.click(); 
+        assertCurrentPageContains("Select Clinical");
+        theForm = theCurrentPage.getFormWithName("clinicalMarkerForm");
+        theForm.setParameter("name", "Progesterone Receptor (PR)");
+        theCurrentPage = theForm.submit();
+        assertCurrentPageContains("You have successfully added a Clinical Marker to this model!");
+        
+        /* Find Clinical Marker link to Edit */
+        theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "Progesterone Receptor");        
+        assertNotNull("Unable to find link to edit the Clinical Marker", theLink);        
+        theCurrentPage = theLink.click();        
+        assertCurrentPageContains("Select Clinical");
+        theForm = theCurrentPage.getFormWithName("clinicalMarkerForm");
+        theForm.setParameter("name", "Progesterone Receptor (PR)");
+        theForm.setParameter("value", "1234567890");          
+        theCurrentPage = theForm.submit();
+        assertCurrentPageContains("You have successfully edited a Clinical Marker.");      
+        
+        /* Find Histopathology link to Delete */
         theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "Heart");
         assertNotNull("Unable to find link to delete the Histopathology", theLink);        
         theCurrentPage = theLink.click();        
@@ -95,42 +126,7 @@ public class SubmitEditDeleteHistopathologyTest extends BaseModelNeededTest {
         theForm = theCurrentPage.getFormWithName("histopathologyForm");               
         theForm.getSubmitButton( "submitAction", "Delete" ).click();              
         assertCurrentPageContains("You have successfully deleted a Histopathology."); 
-        */   
-    } 
-    
-    public void testClinicalMarker() throws Exception {
-    	navigateToModelForEditing(myModelName);
         
-        /* Find Clinical Marker link to Submit 
-        WebLink theLink = myWebConversation.getCurrentPage()
-                .getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "Enter Clinical Marker");
-        WebResponse theCurrentPage = theLink.click(); 
-        assertCurrentPageContains("Select Clinical");
-        WebForm theForm = theCurrentPage.getFormWithName("clinicalMarkerForm");
-        theForm.setParameter("name", "ABCD");
-        theCurrentPage = theForm.submit();
-        assertCurrentPageContains("You have successfully added a Clinical Marker to this model!");
-        */
-        /* Find Model Availablity link to Edit 
-        theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "ABCD");        
-        assertNotNull("Unable to find link to edit the Availability from Jackson Lab", theLink);        
-        theCurrentPage = theLink.click();        
-        assertCurrentPageContains("Select Clinical");
-        theForm = theCurrentPage.getFormWithName("clinicalMarkerForm");
-        theForm.setParameter("name", "ABCD");
-        theForm.setParameter("value", "12345");          
-        theCurrentPage = theForm.submit();
-        assertCurrentPageContains("You have successfully edited a Clinical Marker.");      
-        */
-        /* Find Model Availablity link to Delete 
-        theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "ABCD");
-        assertNotNull("Unable to find link to delete the Availability from Jackson Lab", theLink);        
-        theCurrentPage = theLink.click();        
-        assertCurrentPageContains("Select Clinical");
-        theForm = theCurrentPage.getFormWithName("clinicalMarkerForm");               
-        theForm.getSubmitButton( "submitAction", "Delete" ).click();              
-        assertCurrentPageContains("You have successfully deleted a Clinical Marker."); 
-        */
     }    
 
 }
