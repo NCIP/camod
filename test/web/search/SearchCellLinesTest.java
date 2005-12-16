d6796f3d42a6d062f0a2ca6cbd20767412563527
Sima Pandya <pandyas@mail.nih.gov>
2005-12-16 17:27:34 +0000

    Added a cell line publication to script - there are two issues that need resolved - there is additional source code to upload for this to run
    SVN-Revision: 1577

--- test/web/search/SearchCellLinesTest.java
+++ test/web/search/SearchCellLinesTest.java
@@ -1,14 +1,18 @@
 /**
  * @author pandyas
  * 
- * $Id: SearchCellLinesTest.java,v 1.1 2005-12-13 19:55:49 pandyas Exp $
+ * $Id: SearchCellLinesTest.java,v 1.2 2005-12-16 17:27:34 pandyas Exp $
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2005/12/13 19:55:49  pandyas
+ * JUnit test case for Search Cell Line
+ *
  * 
  */
 package web.search;
 
 import gov.nih.nci.camod.webapp.form.CellLineForm;
+import gov.nih.nci.camod.webapp.form.PublicationForm;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -58,7 +62,7 @@ public class SearchCellLinesTest extends BaseModelNeededTest {
 		// Adding a Cell Line
 		WebLink theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT,
 				"Enter Cell Lines");
-		assertNotNull("Unable to find link to enter a Cell Line", theLink);
+		assertNotNull("Unable to find link to enter a Cell Line", theLink);		
 		WebResponse theCurrentPage = theLink.click();
 		assertCurrentPageContains("Experiment:");
 		WebForm theWebForm = theCurrentPage.getFormWithName("cellLineForm");
@@ -77,6 +81,7 @@ public class SearchCellLinesTest extends BaseModelNeededTest {
 		TestUtil.setValuesOnForm(theForm, theWebForm);
 		
 		theCurrentPage = theWebForm.submit();
+
 		assertCurrentPageContains("You have successfully added a Cell Line to this model!");
 
 		TestUtil.moveModelToEditedApproved(myModelName);
@@ -84,6 +89,68 @@ public class SearchCellLinesTest extends BaseModelNeededTest {
 		navigateToSpecificSearchPage(myModelName,"CELL LINES");
 		
 		verifyValuesOnPage(theWebForm, theParamsToSkip);
+		
+		/******* Adding Publication to cell line ***********/
+		navigateToModelForEditing(myModelName);
+
+		// Adding a Publication
+		theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(
+				WebLink.MATCH_TEXT, "Enter Publication for Cell Line");
+		assertNotNull("Unable to find link to enter a Publication for Cell line", theLink);
+		theCurrentPage = theLink.click();		
+		assertCurrentPageContains("For publications with a PubMed record");
+		theWebForm = theCurrentPage.getFormWithName("publicationForm");
+
+		PublicationForm thePubForm = new PublicationForm();
+		thePubForm.setFirstTimeReported("yes");
+		thePubForm.setPmid("16323327");
+		thePubForm.setYear("1999");
+		thePubForm.setStartPage("1111");
+		thePubForm.setEndPage("9999");		
+		thePubForm.setVolume("10");
+		thePubForm.setTitle("title");
+
+		//TODO: clean up the use of aCellID and ACellID, check ATherapyID and APubID also
+		List theParamsToIgnore = new ArrayList();
+		theParamsToIgnore.add("volume");
+		theParamsToIgnore.add("title");		
+		theParamsToIgnore.add("aCellID");
+		theParamsToIgnore.add("ACellID");		
+		theParamsToIgnore.add("APubID");
+		theParamsToIgnore.add("ATherapyID");		
+
+		//Add parameters found on submit screen but not displayed on search
+		theParamsToSkip = new ArrayList();
+		theParamsToSkip.add("firstTimeReported");
+		theParamsToSkip.add("aCellID");
+		theParamsToSkip.add("ACellID");		
+		theParamsToSkip.add("APubID");
+		theParamsToSkip.add("volume");
+		theParamsToSkip.add("title");
+		theParamsToSkip.add("ATherapyID");		
+		
+
+		TestUtil.setRandomValues(thePubForm, theWebForm, false,	theParamsToIgnore);
+		TestUtil.setValuesOnForm(thePubForm, theWebForm);
+
+		theCurrentPage = theWebForm.submit();
+		//TestUtil.getTextOnPage(theCurrentPage, "Error: Bad or missing data", "* indicates a required field");
+		
+		assertCurrentPageContains("You have successfully added a Publication to this model! ");
+
+		TestUtil.moveModelToEditedApproved(myModelName);
+
+		// check if publication is on cell line page
+		navigateToSpecificSearchPage(myModelName, "CELL LINES");
+		verifyValuesOnPage(theWebForm, theParamsToSkip);
+		
+		/* check if publication is on publication search page
+		* TODO:  This is a bug in the application, the pub only shows up
+		* if a general publication exists for the model in addition
+		* to the cell line publication
+		navigateToSpecificSearchPage(myModelName, "PUBLICATIONS");
+		verifyValuesOnPage(theWebForm, theParamsToSkip);
+		*/
 	}  
 
 }
