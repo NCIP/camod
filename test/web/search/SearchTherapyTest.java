5a1f58a6a6592f1f042673640ee0e251817ea717
Sima Pandya <pandyas@mail.nih.gov>
2005-12-16 17:43:13 +0000

    Added a therapy publication to script - there are two issues that need resolved - there is additional source code to upload for this to run
    SVN-Revision: 1579

--- test/web/search/SearchTherapyTest.java
+++ test/web/search/SearchTherapyTest.java
@@ -1,14 +1,18 @@
 /**
  * @author pandyas
  * 
- * $Id: SearchTherapyTest.java,v 1.1 2005-12-13 20:39:14 pandyas Exp $
+ * $Id: SearchTherapyTest.java,v 1.2 2005-12-16 17:43:13 pandyas Exp $
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2005/12/13 20:39:14  pandyas
+ * JUnit test case for Search Therapy - depends on other changes in main tree that will NOT be uploaded until we go to production
+ *
  * 
  */
 
 package web.search;
 
+import gov.nih.nci.camod.webapp.form.PublicationForm;
 import gov.nih.nci.camod.webapp.form.TherapyForm;
 import java.util.ArrayList;
 import java.util.List;
@@ -92,6 +96,70 @@ public class SearchTherapyTest extends BaseModelNeededTest {
 		navigateToSpecificSearchPage(myModelName,"THERAPEUTIC APPROACHES");
 		
 		verifyValuesOnPage(theWebForm, theParamsToSkip);
+		
+		/******* Adding Publication to Therapy ***********/
+		navigateToModelForEditing(myModelName);
+
+		// Adding a Publication
+		theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(
+				WebLink.MATCH_TEXT, "Enter Publication for Therapy");
+		assertNotNull("Unable to find link to enter a Publication for Therapy", theLink);
+		theCurrentPage = theLink.click();		
+		assertCurrentPageContains("For publications with a PubMed record");
+		theWebForm = theCurrentPage.getFormWithName("publicationForm");
+
+		PublicationForm thePubForm = new PublicationForm();
+		thePubForm.setFirstTimeReported("yes");
+		thePubForm.setPmid("16323327");
+		thePubForm.setYear("1999");
+		thePubForm.setStartPage("1111");
+		thePubForm.setEndPage("9999");		
+		thePubForm.setVolume("10");
+		thePubForm.setTitle("title");
+
+		//TODO: clean up the use of aCellID and ACellID, ATherapyID vs ATherapyID and APubID vs aPubID
+		theParamsToIgnore = new ArrayList();
+		theParamsToIgnore.add("volume");
+		theParamsToIgnore.add("title");		
+		theParamsToIgnore.add("aCellID");
+		theParamsToIgnore.add("ACellID");		
+		theParamsToIgnore.add("APubID");
+		theParamsToIgnore.add("ATherapyID");
+		theParamsToIgnore.add("aTherapyID");		
+
+		//Add parameters found on submit screen but not displayed on search
+		theParamsToSkip = new ArrayList();
+		theParamsToSkip.add("firstTimeReported");
+		theParamsToSkip.add("aCellID");
+		theParamsToSkip.add("ACellID");		
+		theParamsToSkip.add("APubID");
+		theParamsToSkip.add("volume");
+		theParamsToSkip.add("title");
+		theParamsToSkip.add("ATherapyID");
+		theParamsToSkip.add("aTherapyID");		
+		
+
+		TestUtil.setRandomValues(thePubForm, theWebForm, false,	theParamsToIgnore);
+		TestUtil.setValuesOnForm(thePubForm, theWebForm);
+
+		theCurrentPage = theWebForm.submit();
+		//TestUtil.getTextOnPage(theCurrentPage, "Error: Bad or missing data", "* indicates a required field");
+		
+		assertCurrentPageContains("You have successfully added a Publication to this model! ");
+
+		TestUtil.moveModelToEditedApproved(myModelName);
+
+		// check if publication is on cell line page
+		navigateToSpecificSearchPage(myModelName, "THERAPEUTIC APPROACHES");
+		verifyValuesOnPage(theWebForm, theParamsToSkip);
+		
+		/* check if publication is on publication search page
+		* TODO:  This is a bug in the application, the pub only shows up
+		* if a general publication exists for the model in addition
+		* to the therapy publication 
+		navigateToSpecificSearchPage(myModelName, "PUBLICATIONS");
+		verifyValuesOnPage(theWebForm, theParamsToSkip);
+		*/		
 	}  
 
 }
