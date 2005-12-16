27d712e6d70a50d23ee6e5ee8e876d7a5a018e12
Sima Pandya <pandyas@mail.nih.gov>
2005-12-16 18:13:21 +0000

    Added an Assoc Met and Clinical Marker to script - there is one more convenience TODO that would be nice to have
    SVN-Revision: 1580

--- test/web/search/SearchHistopathologyTest.java
+++ test/web/search/SearchHistopathologyTest.java
@@ -1,14 +1,19 @@
 /**
  * @author pandyas
  * 
- * $Id: SearchHistopathologyTest.java,v 1.1 2005-12-14 17:17:42 pandyas Exp $
+ * $Id: SearchHistopathologyTest.java,v 1.2 2005-12-16 18:13:21 pandyas Exp $
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2005/12/14 17:17:42  pandyas
+ * JUnit test case for Search Histopathology - depends on other changes in main tree that will NOT be uploaded until we go to production
+ *
  * 
  */
 
 package web.search;
 
+import gov.nih.nci.camod.webapp.form.AssociatedMetastasisForm;
+import gov.nih.nci.camod.webapp.form.ClinicalMarkerForm;
 import gov.nih.nci.camod.webapp.form.HistopathologyForm;
 import java.util.ArrayList;
 import java.util.List;
@@ -94,6 +99,83 @@ public class SearchHistopathologyTest extends BaseModelNeededTest {
 		navigateToSpecificSearchPage(myModelName,"HISTOPATHOLOGY");
 		
 		verifyValuesOnPage(theWebForm, theParamsToSkip);
+		
+		/******* Adding Assoc Metas to Histopathology ***********/		
+		navigateToModelForEditing(myModelName);
+
+		// Adding an Assoc Metastasis
+		theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT,
+				"Enter Assoc Metastasis");
+		assertNotNull("Unable to find link to add an Assoc Metastasis to Histopathology", theLink);
+		theCurrentPage = theLink.click();
+		assertCurrentPageContains("Age of Metastasis");
+		theWebForm = theCurrentPage.getFormWithName("associatedMetastasisForm");
+
+		AssociatedMetastasisForm theAMForm = new AssociatedMetastasisForm();
+		theAMForm.setOrgan("Heart");
+		theAMForm.setOrganTissueName("Heart");		
+		theAMForm.setOrganTissueCode("C22498");
+		theAMForm.setTumorClassification("Lymphoid_Hyperplasia_of_the_Mouse_Intestinal_Tract");
+		theAMForm.setDiagnosisName("Lymphoid_Hyperplasia_of_the_Mouse_Intestinal_Tract");
+		theAMForm.setDiagnosisCode("C22100");	
+		theAMForm.setVolumeOfTumor("2");
+		theAMForm.setWeightOfTumor("30");
+		theAMForm.setComments("Test Comments");
+		theAMForm.setTumorIncidenceRate("20");
+		
+		//TODO: add check for histopathologyID so we can remove from skip list
+		/* Add parameters found on submit screen but not displayed on search screen  */
+		theParamsToSkip = new ArrayList();		
+		theParamsToSkip.add("organTissueCode");
+		theParamsToSkip.add("organTissueName");
+		theParamsToSkip.add("tumorClassification");
+		theParamsToSkip.add("diagnosisCode");
+		theParamsToSkip.add("diagnosisName");
+		theParamsToSkip.add("methodOfObservation");
+		theParamsToSkip.add("aHistopathologyID");	
+		
+		TestUtil.setRandomValues(theAMForm, theWebForm, false);
+		TestUtil.setValuesOnForm(theAMForm, theWebForm);
+		
+		theCurrentPage = theWebForm.submit();
+		assertCurrentPageContains("You have successfully added an Associated Metastasis to this model!");
+
+		TestUtil.moveModelToEditedApproved(myModelName);
+
+		navigateToSpecificSearchPage(myModelName,"HISTOPATHOLOGY");
+		
+		verifyValuesOnPage(theWebForm, theParamsToSkip);
+		
+		/******* Adding Clinical Marker to Histopathology ***********/		
+		navigateToModelForEditing(myModelName);
+
+		// Adding an Clinical Marker		
+		theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT,
+		"Enter Clinical Marker");
+		assertNotNull("Unable to find link to add a Clinical Marker to Histopathology", theLink);
+		theCurrentPage = theLink.click();
+		assertCurrentPageContains("Select Clinical");
+		theWebForm = theCurrentPage.getFormWithName("clinicalMarkerForm");
+
+		ClinicalMarkerForm theCMForm = new ClinicalMarkerForm();
+
+		//TODO: add check for histopathologyID so we can remove from skip list
+		/* Add parameters found on submit screen but not displayed on search screen  */
+		theParamsToSkip = new ArrayList();		
+		theParamsToSkip.add("aHistopathologyID");
+
+		TestUtil.setRandomValues(theCMForm, theWebForm, false);
+		TestUtil.setValuesOnForm(theCMForm, theWebForm);	
+		
+		theCurrentPage = theWebForm.submit();
+		assertCurrentPageContains("You have successfully added a Clinical Marker to this model!");
+
+		TestUtil.moveModelToEditedApproved(myModelName);
+
+		navigateToSpecificSearchPage(myModelName,"HISTOPATHOLOGY");
+		
+		verifyValuesOnPage(theWebForm, theParamsToSkip);		
+		
 	}  	
 
 }
