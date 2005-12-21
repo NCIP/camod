29e44a571300906ad02b9f17e9f95408b2723ab3
Sima Pandya <pandyas@mail.nih.gov>
2005-12-21 18:00:31 +0000

    Added test for "Other" dropdown options
    SVN-Revision: 1611

--- test/web/search/SearchXenograftTest.java
+++ test/web/search/SearchXenograftTest.java
@@ -1,9 +1,12 @@
 /**
  * @author pandyas
  * 
- * $Id: SearchXenograftTest.java,v 1.1 2005-12-12 19:04:21 pandyas Exp $
+ * $Id: SearchXenograftTest.java,v 1.2 2005-12-21 18:00:31 pandyas Exp $
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2005/12/12 19:04:21  pandyas
+ * first version of JUnit test case
+ *
  * 
  */
 package web.search;
@@ -94,4 +97,51 @@ public class SearchXenograftTest extends BaseModelNeededTest {
 		verifyValuesOnPage(theWebForm);
 	}  
 
+	public void testSearchForXenograftWithOthers() throws Exception {
+
+		navigateToModelForEditing(myModelName);
+
+		// Adding a Transplant/Xenograft
+		WebLink theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT,
+				"Enter Transplant/Xenograft");
+		assertNotNull("Unable to find link to enter a Xenograft", theLink);
+		WebResponse theCurrentPage = theLink.click();
+		assertCurrentPageContains("if graft type is not listed");
+		WebForm theWebForm = theCurrentPage.getFormWithName("xenograftForm");
+
+		XenograftForm theForm = new XenograftForm();
+		theForm.setAdministrativeSite("ear");
+        theForm.setName("ABCDEFG");
+        theForm.setHostScientificName("Mus musculus");         
+        theForm.setGraftType("Cell Line");
+        theForm.setParentalCellLineName("Parent Cell Line");
+        theForm.setHostScientificName("Mus musculus");
+        theForm.setATCCNumber("2");
+        theForm.setCellAmount("10");
+        theForm.setGeneticManipulation("Test Genetic Manipulation");
+        
+		List theParamsToIgnore = new ArrayList();
+		theParamsToIgnore.add("harvestDate");
+		theParamsToIgnore.add("organ");
+		theParamsToIgnore.add("organTissueCode");
+		theParamsToIgnore.add("organTissueName");
+		theParamsToIgnore.add("otherAdministrativeSite");
+		theParamsToIgnore.add("otherGraftType");
+		theParamsToIgnore.add("hostEthinicityStrain");		
+		theParamsToIgnore.add("otherHostEthinicityStrain");
+		theParamsToIgnore.add("modificationDescription");		
+		
+		TestUtil.setRandomValues(theForm, theWebForm, true, theParamsToIgnore);
+		TestUtil.setValuesOnForm(theForm, theWebForm);
+		
+		theCurrentPage = theWebForm.submit();
+		assertCurrentPageContains("You have successfully added a Xenograft to this model!");
+
+		TestUtil.moveModelToEditedApproved(myModelName);
+
+		navigateToSpecificSearchPage(myModelName,"TRANSPLANT/XENOGRAFT");
+		
+		verifyValuesOnPage(theWebForm);
+	} 	
+	
 }
