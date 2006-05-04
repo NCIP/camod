d3dfc4097bcfb6897648bbf9ce42cd0da0e47086
schroedn <schroedn>
2006-05-04 14:12:13 +0000

    Changes from code review
    SVN-Revision: 1994

--- src/gov/nih/nci/camod/domain/SavedQueryAttribute.java
+++ src/gov/nih/nci/camod/domain/SavedQueryAttribute.java
@@ -1,8 +1,12 @@
 /**
  * 
- * $Id: SavedQueryAttribute.java,v 1.1 2006-04-28 19:12:44 schroedn Exp $
+ * $Id: SavedQueryAttribute.java,v 1.2 2006-05-04 14:12:13 schroedn Exp $
  * 
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2006/04/28 19:12:44  schroedn
+ * Defect #238, 261
+ * Search Result Columns and Saving Queries domain objects and hibernate mapping files
+ *
  *
  */
 
@@ -10,83 +14,52 @@ package gov.nih.nci.camod.domain;
 
 import java.io.Serializable;
 
+/**
+ * @author schroedn
+ */
 public class SavedQueryAttribute extends BaseObject implements Serializable
 {
-    // Pointer to this object's parent  
-    private SavedQuery parentQuery;
     
     // Name of the attribute is the fully qualified classname (subclass of criteria)
     private String attributeName;
     
-    // Used to store the order of the attributes
-    private String subAttributeName;
-    
     // Value of the criteria
     private String attributeValue; 
     
-    /**
-     * 
-     */
     private static final long serialVersionUID = 2581622057143043273L;
-    
+
+
     /**
-     * 
-     *
+     * @return Returns the attributeName setting.
      */
-    public SavedQueryAttribute()
-    {
-    }    
-    
-    /**
-     * Creates an object based on a QueryAttributeWrapper
-     * 
-     * @param wrapper
-     * @param parent
-     */        
-    public SavedQueryAttribute(QueryAttributeWrapper wrapper, SavedQuery parent)
-    {
-        setAttributeName(wrapper.getCriteriaClassName());
-        setAttributeValue(wrapper.getAttributeValue());
-        setSubAttributeName(wrapper.getSubAttributeName());
-        setParentQuery(parent);
-    }
-    
-    public SavedQuery getParentQuery() {
-        return parentQuery;
-    }
-
-    public void setParentQuery(SavedQuery parentQuery) {
-        this.parentQuery = parentQuery;
-    }
-
     public String getAttributeName()
     {
         return attributeName;
     }
 
+    /**
+     * @param attributeName
+     *     sets the attributeName
+     */
     public void setAttributeName(String attributeName)
     {
         this.attributeName = attributeName;
     }
 
+    /**
+     * @return Returns the attributeValue setting.
+     */
     public String getAttributeValue()
     {
         return attributeValue;
     }
 
+    /**
+     * @param attributeValue
+     *     sets the attributeValue
+     */
     public void setAttributeValue(String attributeValue)
     {
         this.attributeValue = attributeValue;
     }
-
-    public String getSubAttributeName()
-    {
-        return subAttributeName;
-    }
-
-    public void setSubAttributeName(String subAttributeName)
-    {
-        this.subAttributeName = subAttributeName;
-    }       
-       
 }
