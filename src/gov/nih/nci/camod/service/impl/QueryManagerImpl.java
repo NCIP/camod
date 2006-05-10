601849e4f60269c6a9cc65524f2e59d8fe12c28d
georgeda <georgeda>
2006-05-10 12:00:39 +0000

    Changes for searching on transient interfaces, and fixed a bug from 2.1 OM change
    SVN-Revision: 2025

--- src/gov/nih/nci/camod/service/impl/QueryManagerImpl.java
+++ src/gov/nih/nci/camod/service/impl/QueryManagerImpl.java
@@ -1,9 +1,54 @@
 /**
- * @author dgeorge
  * 
- * $Id: QueryManagerImpl.java,v 1.43 2006-05-05 17:16:44 georgeda Exp $
+ *   The caMOD Software License, Version 1.0
+ *
+ *   Copyright 2005-2006 SAIC. This software was developed in conjunction with the National Cancer
+ *   Institute, and so to the extent government employees are co-authors, any rights in such works
+ *   shall be subject to Title 17 of the United States Code, section 105.
+ *
+ *   Redistribution and use in source and binary forms, with or without modification, are permitted
+ *   provided that the following conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ *   and the disclaimer of Article 3, below.  Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ *   2.  The end-user documentation included with the redistribution, if any, must include the
+ *   following acknowledgment:
+ *
+ *   "This product includes software developed by the SAIC and the National Cancer
+ *   Institute."
+ *
+ *   If no such end-user documentation is to be included, this acknowledgment shall appear in the
+ *   software itself, wherever such third-party acknowledgments normally appear.
+ *
+ *   3. The names "The National Cancer Institute", "NCI" and "SAIC" must not be used to endorse or
+ *   promote products derived from this software.
+ *
+ *   4. This license does not authorize the incorporation of this software into any third party proprietary
+ *   programs.  This license does not authorize the recipient to use any trademarks owned by either
+ *   NCI or SAIC.
+ *
+ *
+ *   5. THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESSED OR IMPLIED
+ *   WARRANTIES, (INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE) ARE
+ *   DISCLAIMED.  IN NO EVENT SHALL THE NATIONAL CANCER INSTITUTE, SAIC, OR
+ *   THEIR AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ *   OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * $Id: QueryManagerImpl.java,v 1.44 2006-05-10 12:00:39 georgeda Exp $
  * 
  * $Log: not supported by cvs2svn $
+ * Revision 1.43  2006/05/05 17:16:44  georgeda
+ * Fixed one more error w/ SQL introduced during model change
+ *
  * Revision 1.42  2006/05/05 16:57:02  georgeda
  * Fixed errors w/ SQL introduced during model change
  *
@@ -1168,13 +1213,30 @@ public class QueryManagerImpl extends BaseManager
     private String getModelIdsForMicroArrayData() throws PersistenceException
     {
 
-        String theSQLString = "SELECT distinct abs_cancer_model_id FROM ani_mod_mic_array_data";
+        String theSQLString = "SELECT distinct abs_cancer_model_id FROM micro_array_data";
 
         Object[] theParams = new Object[0];
         return getIds(theSQLString, theParams);
 
     }
 
+    /**
+     * Get the model id's for any model that has associated transient interface data
+     * 
+     * @return a list of matching model ids
+     * 
+     * @throws PersistenceException
+     */
+    private String getModelIdsForTransientInterference() throws PersistenceException
+    {
+
+        String theSQLString = "SELECT distinct abs_cancer_model_id FROM morpholino";
+
+        Object[] theParams = new Object[0];
+        return getIds(theSQLString, theParams);
+
+    }
+    
     /**
      * Get the model id's for any model that has a cellline w/ a matching name
      * 
@@ -1224,6 +1286,34 @@ public class QueryManagerImpl extends BaseManager
 
     }
 
+    /**
+     * Get the model id's for any model that has a transient interference associated
+     * with a specific organ.
+     * 
+     * @param inTransientInterference
+     *            the transient interface to search for
+     * 
+     * @return a list of matching model id
+     * 
+     * @throws PersistenceException
+     */
+    private String getModelIdsForTransientInterference(String inTransientInterference) throws PersistenceException
+    {
+
+        String theSQLString = "SELECT distinct m.abs_cancer_model_id " + "FROM morpholino m WHERE upper(m.targeted_region) like ?)";
+
+        String theSQLTheraputicApproach = "%";
+        if (inTransientInterference != null && inTransientInterference.trim().length() > 0)
+        {
+            theSQLTheraputicApproach = "%" + inTransientInterference.trim().toUpperCase() + "%";
+        }
+
+        Object[] theParams = new Object[1];
+        theParams[0] = theSQLTheraputicApproach;
+        return getIds(theSQLString, theParams);
+
+    }
+    
     /**
      * Get the model id's for any model that has a histopathology associated
      * with a specific organ.
@@ -1540,6 +1630,8 @@ public class QueryManagerImpl extends BaseManager
         theWhereClause += " OR abs_cancer_model_id IN (" + getModelIdsForCellLine(inKeyword) + ")";
 
         theWhereClause += " OR abs_cancer_model_id IN (" + getModelIdsForTherapeuticApproach(inKeyword) + "))";
+        
+        theWhereClause += " OR abs_cancer_model_id IN (" + getModelIdsForTransientInterference(inKeyword) + "))";
 
         return theWhereClause;
     }
@@ -1720,7 +1812,13 @@ public class QueryManagerImpl extends BaseManager
         {
             theWhereClause += " AND abs_cancer_model_id IN (" + getModelIdsForMicroArrayData() + ")";
         }
-
+        
+        // Search for microarray data
+        if (inSearchData.isSearchTransientInterference())
+        {
+            theWhereClause += " AND abs_cancer_model_id IN (" + getModelIdsForTransientInterference() + ")";
+        }
+        
         // Search for xenograft
         if (inSearchData.isSearchXenograft())
         {
@@ -1735,7 +1833,6 @@ public class QueryManagerImpl extends BaseManager
                                 String inOrderByClause,
                                 SearchData inSearchData) throws Exception
     {
-
         String theWhereClause = buildCriteriaSearchWhereClause(inSearchData);
 
         List theAnimalModels = null;
@@ -1745,9 +1842,7 @@ public class QueryManagerImpl extends BaseManager
             String theHQLQuery = inFromClause + theWhereClause + inOrderByClause;
 
             log.info("HQL Query: " + theHQLQuery);
-
-            // HQLParameter[] theParameters = new HQLParameter[0];
-            // animalModels = Search.query(theHQLQuery, theParameters);
+            
             Query theQuery = HibernateUtil.getSession().createQuery(theHQLQuery);
             theAnimalModels = theQuery.list();
         }
