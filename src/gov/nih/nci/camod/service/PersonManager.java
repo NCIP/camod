524468b5e6e72d81f6db17ecc7b1462e45f48e3c
Sima Pandya <pandyas@mail.nih.gov>
2006-05-22 20:13:19 +0000

    Removed TODO text
    SVN-Revision: 2137

--- src/gov/nih/nci/camod/service/PersonManager.java
+++ src/gov/nih/nci/camod/service/PersonManager.java
@@ -1,8 +1,6 @@
 /*
  * Created on Jun 17, 2005
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 package gov.nih.nci.camod.service;
 
