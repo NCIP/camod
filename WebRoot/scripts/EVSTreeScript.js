2122ca95bacbba6a1bdf2544657461792d218875
Sima Pandya <pandyas@mail.nih.gov>
2006-09-07 14:43:57 +0000

    new javascript from rewrite of EVSTree (new cache functionality)
    SVN-Revision: 2316

--- WebRoot/scripts/EVSTreeScript.js
+++ WebRoot/scripts/EVSTreeScript.js
@@ -1,108 +1,114 @@
-var skin = 'evsMOD';
-windowTitle = "NCI Center for Bioinformatics";
+    var skin = 'evsMOD';   
 
-// targetFormName (String) - The name of the form with the target form fields (organ, organTissueName, organTissueCode) * NO SPACES *
-// species (String) - desired species * NO SPACES *
+    windowTitle = "NCI Center for Bioinformatics";    
 
-function showMouseTissueTree(form, inConceptCode, inConceptName, inDisplayName, leafNode)
-{
-	var rootNode = 'Murine_Tissue_Type';
-	var roleType= '';
-	var sementicType = 'T023,T024,T025';
-	var title = 'Tissue Select';
-	var params = ["true", "true", "-1", "0"];
-	var postMsg = false;
-
-	showTree(form, inConceptCode, inConceptName, inDisplayName, rootNode, roleType, sementicType, skin, params, leafNode, title, '', postMsg, '')
-}
-
-function showMouseDiagnosisTree(form, inConceptCode, inConceptName, inDisplayName, leafNode, rootNode)
-{
-	var rootNode = 'Mouse_Disorder_by_Site';
-	var roleType= '';
-	var sementicType = 'T047,T191';
-	var title = 'Diagnosis Select';
-	var params = ["true", "true", "-1", "0"];
-	var postMsg = true;
-
-	showTree(form, inConceptCode, inConceptName, inDisplayName, rootNode, roleType, sementicType,  skin, params, leafNode, title, '', postMsg, '')		
-}
-
-function showHumanTissueTree(form, inConceptCode, inConceptName, inDisplayName, leafNode)
-{
-	var rootNode = 'Organ_System';
-	var roleType= 'Anatomic_Structure_is_Physical_Part_Of';
-	var sementicType = 'T023,T017';
-	var title = 'Human Tissue Select';
-	var params = ["true", "true", "-1", "0"];
-	var postMsg = false;
-
-	showTree(form, inConceptCode, inConceptName, inDisplayName, rootNode, roleType, sementicType, skin, params, leafNode, title, '', postMsg, '')
-}
-
-function showTree(form, conceptCode, conceptName, displayName, rootNode, roleType, sementicType,  skin, params, leafNode, title, fieldsToBlank, postMsg, rootTop)
-{
-    var now = new Date();
-    var glob = now.getHours()+now.getSeconds()+now.getMilliseconds();
-    var targetURL;
-    var descendants;
-    var isaFlag;
-    var depthLevel;
-    var attribute;
- 
-    descendants = params[0];
-    isaFlag = params[1];
-    depthLevel = params[2];
-    attribute = params[3];
-
-    if((fieldsToBlank==undefined)||(fieldsToBlank==''))
+    // show tissue tree with fieldToBlank parameter
+    function showMouseTissueTree(form, inConceptCode, inConceptName, inDisplayName, onlyLeaf, fieldsToBlank)
     {
-        if((rootTop==undefined)||(rootTop==''))
-        {
-	        if((roleType==undefined)||(roleType==''))
-	        {
-	            targetURL = '/IEVSTree/webtree/WebTreeMain.jsp?treeParams=rootNode='+rootNode+';conceptName='+conceptName+';conceptCode='+conceptCode+';displayName='+displayName+';descendants='+descendants+';isaFlag='+isaFlag+';depthLevel='+depthLevel+';attributes='+attribute+';onlyLeaf='+leafNode+';sementicType='+sementicType+';formName='+form+';postMsg='+postMsg+';&skin='+skin+'&windowTitle='+title+'&rand='+glob;
-	        } 
-	        else 
-	        {
-	            targetURL = '/IEVSTree/webtree/WebTreeMain.jsp?treeParams=rootNode='+rootNode+';conceptName='+conceptName+';conceptCode='+conceptCode+';displayName='+displayName+';descendants='+descendants+';isaFlag='+isaFlag+';depthLevel='+depthLevel+';attributes='+attribute+';onlyLeaf='+leafNode+';roleType='+roleType+';sementicType='+sementicType+';formName='+form+';postMsg='+postMsg+';&skin='+skin+'&windowTitle='+title+'&rand='+glob;
-	        }
-        }
-        else 
-        {
-            targetURL = '/IEVSTree/webtree/WebTreeMain.jsp?treeParams=rootNode='+rootNode+';rootTop='+rootTop+';conceptName='+conceptName+';conceptCode='+conceptCode+';displayName='+displayName+';descendants='+descendants+';isaFlag='+isaFlag+';depthLevel='+depthLevel+';attributes='+attribute+';onlyLeaf='+leafNode+';roleType='+roleType+';sementicType='+sementicType+';formName='+form+';postMsg='+postMsg+';&skin='+skin+'&windowTitle='+title+'&rand='+glob;
+	var paramsHT = {};
 
-        }
-    }
-    else 
-    {
-        if((roleType==undefined)||(roleType==''))
-        {
-		    targetURL = '/IEVSTree/webtree/WebTreeMain.jsp?treeParams=rootNode='+rootNode+';conceptName='+conceptName+';conceptCode='+conceptCode+';displayName='+displayName+';descendants='+descendants+';isaFlag='+isaFlag+';depthLevel='+depthLevel+';attributes='+attribute+';onlyLeaf='+leafNode+';sementicType='+sementicType+';formName='+form+';fieldsToBlank='+fieldsToBlank+';postMsg='+postMsg+';&skin='+skin+'&windowTitle='+title+'&rand='+glob;
-		} 
-		else 
-		{
-		    targetURL = '/IEVSTree/webtree/WebTreeMain.jsp?treeParams=rootNode='+rootNode+';conceptName='+conceptName+';conceptCode='+conceptCode+';displayName='+displayName+';descendants='+descendants+';isaFlag='+isaFlag+';depthLevel='+depthLevel+';attributes='+attribute+';onlyLeaf='+leafNode+';roleType='+roleType+';sementicType='+sementicType+';formName='+form+';fieldsToBlank='+fieldsToBlank+';postMsg='+postMsg+';&skin='+skin+'&windowTitle='+title+'&rand='+glob;
+        paramsHT.treeNameKey = 'MouseTissue';
+		paramsHT.formName = form;
+		paramsHT.conceptCode = inConceptCode;
+		paramsHT.conceptName = inConceptName;
+		paramsHT.displayName = inDisplayName;		
+        paramsHT.postMsg = false;
+        paramsHT.onlyLeaf = onlyLeaf;   
+        if (fieldsToBlank !== undefined) {
+        	paramsHT.fieldsToBlank = fieldsToBlank;
 		}
-    }
-  
-    // open target window
-    windowOpen(targetURL, 810, 500, title);
-}
+        paramsHT.windowTitle = 'Tissue Select';
 
- // pop-up window utilitiy function
- function windowOpen(url,w,h,title)
- {
-     window.name = 'root';
-     remote = window.open(url,'none','width= '+w+',height='+h+', resizable=yes,scrollbars=yes');
-     if (remote != null) 
-     {
-         remote.title = title;
-         if (remote.opener == null) 
-         {
-             remote.opener = self;
-             remote.name = 'popup';
-         }
+	showTree(paramsHT);
+    }
+
+    // show diagnosis tree with rootConceptNode parameter
+    function showMouseDiagnosisTree(form, inConceptCode, inConceptName, inDisplayName, onlyLeaf)
+    {
+        var paramsHT = {};
+
+        paramsHT.treeNameKey = 'MouseDiagnosis';        
+		paramsHT.formName = form;
+		paramsHT.conceptCode = inConceptCode;
+		paramsHT.conceptName = inConceptName;
+		paramsHT.displayName = inDisplayName;
+        paramsHT.onlyLeaf = onlyLeaf;           
+        paramsHT.windowTitle = 'Diagnosis Select';        
+        paramsHT.postMsg = true;
+
+        showTree(paramsHT);		
      }
-     remote.focus();
-}
+
+    function showHumanTissueTree(form, inConceptCode, inConceptName, inDisplayName, onlyLeaf, fieldsToBlank)	
+    {      
+	var paramsHT = {};
+     
+        paramsHT.treeNameKey = 'HumanTissue';
+		paramsHT.formName = form;
+		paramsHT.conceptCode = inConceptCode;
+		paramsHT.conceptName = inConceptName;
+		paramsHT.displayName = inDisplayName;        	
+        paramsHT.postMsg = false;
+        paramsHT.onlyLeaf = onlyLeaf;        
+        if (fieldsToBlank !== undefined) {
+        	paramsHT.fieldsToBlank = fieldsToBlank;
+        }
+
+        paramsHT.windowTitle = 'Human Tissue Select';
+
+	showTree(paramsHT);
+    }
+
+    function showHumanDiagnosisTree(form, inConceptCode, inConceptName, inDisplayName, onlyLeaf)	
+    {      
+	var paramsHT = {};
+     
+        paramsHT.treeNameKey = 'HumanDiagnosis';
+		paramsHT.formName = form;
+		paramsHT.conceptCode = inConceptCode;
+		paramsHT.conceptName = inConceptName;
+		paramsHT.displayName = inDisplayName;        	
+        paramsHT.postMsg = false;
+        paramsHT.onlyLeaf = onlyLeaf;       
+
+        paramsHT.windowTitle = 'Human Diagnosis Select';
+
+	showTree(paramsHT);
+    }
+
+    // new showTree function (dynamically builds name=value parameter string for url from hashtable)
+    function showTree(paramsHT) 
+    { 
+	var now = new Date();
+        var glob = now.getHours()+now.getSeconds()+now.getMilliseconds();
+        paramsHT.rand = glob;
+        
+        var treeParams = "";
+        for (var paramName in paramsHT) {
+           treeParams += paramName+"="+paramsHT[paramName]+";";
+        }
+      
+	targetURL = '/EVSTree/webtree/WebTreeMain.jsp?treeParams='+treeParams+'&skin='+skin;
+
+	// open target window
+        windowOpen(targetURL, 810, 500, paramsHT.windowTitle);
+    }
+
+
+    // pop-up window utilitiy function
+    function windowOpen(url,w,h,title)
+    {
+      window.name = 'root';
+      remote = window.open(url,'none','width= '+w+',height='+h+', resizable=yes,scrollbars=yes');
+      //remote = window.open(url);
+	if (remote !== null) {
+  		remote.title = title;
+        if (remote.opener == null) {
+          remote.opener = self;
+          remote.name = 'popup';
+        }
+      }
+      remote.focus();
+    }
+
+
