d559e32d59a2d7ab6f08f5274774c766f4e99df2
georgeda <georgeda>
2006-09-11 16:15:35 +0000

    latest copy
    SVN-Revision: 2318

--- WebRoot/scripts/EVSTreeScript.js
+++ WebRoot/scripts/EVSTreeScript.js
@@ -5,96 +5,90 @@
     // show tissue tree with fieldToBlank parameter
     function showMouseTissueTree(form, inConceptCode, inConceptName, inDisplayName, onlyLeaf, fieldsToBlank)
     {
-      var paramsHT = {};
+	var paramsHT = {};
 
-      paramsHT.treeNameKey = 'MouseTissue';
-      paramsHT.formName = form;
-      paramsHT.conceptCode = inConceptCode;
-      paramsHT.conceptName = inConceptName;
-      paramsHT.displayName = inDisplayName;		
-      paramsHT.postMsg = false;
-      paramsHT.onlyLeaf = onlyLeaf;   
-      if (fieldsToBlank != undefined) { 
-		paramsHT.fieldsToBlank = fieldsToBlank;
-	  }
-      paramsHT.windowTitle = 'Tissue Select';
+        paramsHT.treeNameKey = 'MouseTissue';
+	paramsHT.formName = form;
+	paramsHT.conceptCode = inConceptCode;
+	paramsHT.conceptName = inConceptName;
+	paramsHT.displayName = inDisplayName;		
+        paramsHT.postMsg = false;
+        paramsHT.onlyLeaf = onlyLeaf;   
+        if (fieldsToBlank != undefined) paramsHT.fieldsToBlank = fieldsToBlank;
 
-      showTree(paramsHT);
+        paramsHT.windowTitle = 'Tissue Select';
+
+	showTree(paramsHT);
     }
 
     // show diagnosis tree with rootConceptNode parameter
-    function showMouseDiagnosisTree(form, inConceptCode, inConceptName, inDisplayName, onlyLeaf, fieldsToBlank)
+    function showMouseDiagnosisTree(form, inConceptCode, inConceptName, inDisplayName, onlyLeaf)
     {
-      var paramsHT = {};
+        var paramsHT = {};
 
-      paramsHT.treeNameKey = 'MouseDiagnosis';        
-      paramsHT.formName = form;
-      paramsHT.conceptCode = inConceptCode;
-      paramsHT.conceptName = inConceptName;
-      paramsHT.displayName = inDisplayName;
-      paramsHT.onlyLeaf = onlyLeaf;           
-      paramsHT.windowTitle = 'Diagnosis Select';        
-      paramsHT.postMsg = true;
-      if (fieldsToBlank != undefined) {
-		paramsHT.fieldsToBlank = fieldsToBlank;
-	  }
-      showTree(paramsHT);		
+        paramsHT.treeNameKey = 'MouseDiagnosis';        
+	paramsHT.formName = form;
+	paramsHT.conceptCode = inConceptCode;
+	paramsHT.conceptName = inConceptName;
+	paramsHT.displayName = inDisplayName;
+        paramsHT.onlyLeaf = onlyLeaf;           
+        paramsHT.windowTitle = 'Diagnosis Select';        
+        paramsHT.postMsg = true;
+
+        showTree(paramsHT);		
      }
 
     function showHumanTissueTree(form, inConceptCode, inConceptName, inDisplayName, onlyLeaf, fieldsToBlank)	
     {      
-      var paramsHT = {};
+	var paramsHT = {};
      
-      paramsHT.treeNameKey = 'HumanTissue';
-      paramsHT.formName = form;
-      paramsHT.conceptCode = inConceptCode;
-      paramsHT.conceptName = inConceptName;
-      paramsHT.displayName = inDisplayName;        	
-      paramsHT.postMsg = false;
-      paramsHT.onlyLeaf = onlyLeaf;        
-      if (fieldsToBlank != undefined) {
-	  	paramsHT.fieldsToBlank = fieldsToBlank;
-	  }
-      paramsHT.windowTitle = 'Human Tissue Select';
+        paramsHT.treeNameKey = 'HumanTissue';
+	paramsHT.formName = form;
+	paramsHT.conceptCode = inConceptCode;
+	paramsHT.conceptName = inConceptName;
+	paramsHT.displayName = inDisplayName;        	
+        paramsHT.postMsg = false;
+        paramsHT.onlyLeaf = onlyLeaf;        
+        if (fieldsToBlank != undefined) paramsHT.fieldsToBlank = fieldsToBlank;
 
-      showTree(paramsHT);
+        paramsHT.windowTitle = 'Human Tissue Select';
+
+	showTree(paramsHT);
     }
 
-    function showHumanDiagnosisTree(form, inConceptCode, inConceptName, inDisplayName, onlyLeaf, fieldsToBlank)	
+    function showHumanDiagnosisTree(form, inConceptCode, inConceptName, inDisplayName, onlyLeaf)	
     {      
-      var paramsHT = {};
+	var paramsHT = {};
      
-      paramsHT.treeNameKey = 'HumanDiagnosis';
-      paramsHT.formName = form;
-      paramsHT.conceptCode = inConceptCode;
-      paramsHT.conceptName = inConceptName;
-      paramsHT.displayName = inDisplayName;        	
-      paramsHT.postMsg = false;
-      paramsHT.onlyLeaf = onlyLeaf;       
-      if (fieldsToBlank != undefined) {
-		paramsHT.fieldsToBlank = fieldsToBlank;
-	  }
-      paramsHT.windowTitle = 'Human Diagnosis Select';
+        paramsHT.treeNameKey = 'HumanDiagnosis';
+	paramsHT.formName = form;
+	paramsHT.conceptCode = inConceptCode;
+	paramsHT.conceptName = inConceptName;
+	paramsHT.displayName = inDisplayName;        	
+        paramsHT.postMsg = false;
+        paramsHT.onlyLeaf = onlyLeaf;       
 
-      showTree(paramsHT);
+        paramsHT.windowTitle = 'Human Diagnosis Select';
+
+	showTree(paramsHT);
     }
 
     // new showTree function (dynamically builds name=value parameter string for url from hashtable)
     function showTree(paramsHT) 
     { 
-      var now = new Date();
-      var glob = now.getHours()+now.getSeconds()+now.getMilliseconds();
-      paramsHT.rand = glob;
+	var now = new Date();
+        var glob = now.getHours()+now.getSeconds()+now.getMilliseconds();
+        paramsHT.rand = glob;
         
-      var treeParams = "";
-      for (var paramName in paramsHT) {
-         treeParams += paramName+"="+paramsHT[paramName]+";";
-      }
+        var treeParams = "";
+        for (var paramName in paramsHT) {
+           treeParams += paramName+"="+paramsHT[paramName]+";";
+        }
       
-      targetURL = '/EVSTree/webtree/WebTreeMain.jsp?treeParams='+treeParams+'&skin='+skin;
+	targetURL = '/EVSTree/webtree/WebTreeMain.jsp?treeParams='+treeParams+'&skin='+skin;
 
-      // open target window
-      windowOpen(targetURL, 810, 500, paramsHT.windowTitle);
+	// open target window
+        windowOpen(targetURL, 810, 500, paramsHT.windowTitle);
     }
 
 
@@ -102,22 +96,16 @@
     function windowOpen(url,w,h,title)
     {
       window.name = 'root';
-      remote = window.open(url,'popup','width= '+w+',height='+h+', resizable=yes,scrollbars=yes,status=no');
+      remote = window.open(url,'none','width= '+w+',height='+h+', resizable=yes,scrollbars=yes');
       //remote = window.open(url);
-      if (remote != null) {
-        remote.title = title;        
-        if (remote.opener == null) {        
-          remote.opener = self;          
+	if (remote != null) {
+  		remote.title = title;
+        if (remote.opener == null) {
+          remote.opener = self;
+          remote.name = 'popup';
         }
       }
       remote.focus();
     }
 
-    // wrapper functions for caImage project
-   function showTissueTree(form, inConceptCode, inConceptName, inDisplayName, onlyLeaf, fieldsToBlank) {
-       showMouseTissueTree(form, inConceptCode, inConceptName, inDisplayName, onlyLeaf, fieldsToBlank)  
-   }
 
-   function showDiagnosisTree(form, inConceptCode, inConceptName, inDisplayName, onlyLeaf, fieldsToBlank) {
-     showMouseDiagnosisTree(form, inConceptCode, inConceptName, inDisplayName, onlyLeaf, fieldsToBlank)
-   }
