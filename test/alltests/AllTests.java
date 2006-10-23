e2c5205412b777625d7680fe138e652bdc5a127b
Sima Pandya <pandyas@mail.nih.gov>
2006-10-23 17:08:25 +0000

    modified
    SVN-Revision: 2438

--- test/alltests/AllTests.java
+++ test/alltests/AllTests.java
@@ -42,9 +42,12 @@
  *	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $Id: AllTests.java,v 1.2 2005-12-28 16:41:57 georgeda Exp $
+ * $Id: AllTests.java,v 1.3 2006-10-23 17:08:25 pandyas Exp $
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2005/12/28 16:41:57  georgeda
+ * Changes for testing
+ *
  * Revision 1.1  2005/12/27 15:04:33  georgeda
  * Test cleanup
  *
@@ -56,9 +59,9 @@ package alltests;
  */
 import gov.nih.nci.camod.webapp.action.StrutsActionTests;
 import junit.framework.Test;
+import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import junit.textui.TestRunner;
-import web.HttpTests;
 
 public class AllTests
 {
