a5c3fb16beacc5a90c8a6768994949088170392c
Sima Pandya <pandyas@mail.nih.gov>
2006-10-23 17:46:33 +0000

    fixed morpholino test script -  need to fix siRNA script
    SVN-Revision: 2441

--- test/web/search/SearchPopulateTransIntTest.java
+++ test/web/search/SearchPopulateTransIntTest.java
@@ -58,7 +58,7 @@ public class SearchPopulateTransIntTest extends BaseModelNeededTest
         assertCurrentPageContains("if source is not listed");
         System.out.println("here: " );
         
-        WebForm theWebForm = theCurrentPage.getFormWithName("morpholinoForm");
+        WebForm theWebForm = theCurrentPage.getFormWithName("transientInterferenceForm");
 
         TransientInterferenceForm theForm = new TransientInterferenceForm();
         theForm.setSource("Gene Tool");
@@ -84,11 +84,12 @@ public class SearchPopulateTransIntTest extends BaseModelNeededTest
         theCurrentPage = theLink.click();
         
         assertCurrentPageContains("if source is not listed");
-        theWebForm = theCurrentPage.getFormWithName("morpholinoForm");
+        theWebForm = theCurrentPage.getFormWithName("transientInterferenceForm");
 
         //Add parameters found behind but not populate screen
         theParamsToSkip = new ArrayList<String>();
-        theParamsToSkip.add("aMorpholinoID");
+        theParamsToSkip.add("aTransIntID");
+        theParamsToSkip.add("aConceptCode");
         theParamsToSkip.add("otherDeliveryMethod");
         // shows up as a match but maybe font is not read
         theParamsToSkip.add("sequenceDirection");
@@ -100,7 +101,7 @@ public class SearchPopulateTransIntTest extends BaseModelNeededTest
 
     }
 
-    /*
+/*
     public void testPopulateSirna() throws Exception
     {
 
@@ -111,15 +112,15 @@ public class SearchPopulateTransIntTest extends BaseModelNeededTest
         WebLink theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "Enter SiRNA");
         System.out.println("theLink: " + theLink);
         
-        assertNotNull("Unable to find link to enter a Morpholino", theLink);
+        assertNotNull("Unable to find link to enter a SiRNA", theLink);
         WebResponse theCurrentPage = theLink.click();
         assertCurrentPageContains("if source is not listed");
         System.out.println("here: " );
         
-        WebForm theWebForm = theCurrentPage.getFormWithName("morpholinoForm");
+        WebForm theWebForm = theCurrentPage.getFormWithName("transientInterferenceForm");
 
-        MorpholinoForm theForm = new MorpholinoForm();
-        theForm.setSource("Gene Tool");
+        TransientInterferenceForm theForm = new TransientInterferenceForm();
+        theForm.setSource("Amaxa");
         theForm.setTargetedRegion("TESTINGTARGETEDREGION");
 
         List<String> theParamsToIgnore = new ArrayList<String>();
@@ -132,21 +133,22 @@ public class SearchPopulateTransIntTest extends BaseModelNeededTest
 
         theCurrentPage = theWebForm.submit();
 
-        assertCurrentPageContains("You have successfully added a Morpholino to this model! ");
+        assertCurrentPageContains("You have successfully added a siRNA to this model! ");
 
         // Verify that populate method returns complete and correct data
         navigateToModelForEditing(myModelName);
 
         theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "TESTINGTARGETEDREGION");
-        assertNotNull("Unable to find link to verify Morpholino", theLink);
+        assertNotNull("Unable to find link to verify SiRNA", theLink);
         theCurrentPage = theLink.click();
         
         assertCurrentPageContains("if source is not listed");
-        theWebForm = theCurrentPage.getFormWithName("morpholinoForm");
+        theWebForm = theCurrentPage.getFormWithName("transientInterferenceForm");
 
         //Add parameters found behind but not populate screen
         theParamsToSkip = new ArrayList<String>();
-        theParamsToSkip.add("aMorpholinoID");
+        theParamsToSkip.add("aTransIntID");
+        theParamsToSkip.add("aConceptCode");        
         theParamsToSkip.add("otherDeliveryMethod");
         // shows up as a match but maybe font is not read
         theParamsToSkip.add("sequenceDirection");
@@ -155,7 +157,7 @@ public class SearchPopulateTransIntTest extends BaseModelNeededTest
         theParamsToSkip.add("otherVisualLigand");
 
         verifyValuesOnPopulatePage(theWebForm, theParamsToSkip);
-
     }
     */
+
 }
