f4da19a159396f26d0af15c25139476883a7ddc2
no_author <no_author@no_author.cvs2svn>
2006-11-21 22:50:54 +0000

    This commit was manufactured by cvs2svn to create tag 'camod_2_2_0_0_deploy-stage_build_37'.
    SVN-Revision: 2562

--- src/gov/nih/nci/camod/webapp/form/TransgeneData.java
+++
@@ -1,9 +0,0 @@
-package gov.nih.nci.camod.webapp.form;
-
-public interface TransgeneData
-{
-
-    public String getConstructSequence();
-
-    public void setConstructSequence(String constructSequence);
-}
