204b3ccc36c9cfad007efa2160d3d09b9020ca53
no_author <no_author@no_author.cvs2svn>
2007-08-01 18:11:47 +0000

    This commit was manufactured by cvs2svn to create tag 'camod_dev_2_3_deploy-stage_build_1'.
    SVN-Revision: 3048

--- src/gov/nih/nci/camod/service/impl/ImageManagerImpl.java
+++ src/gov/nih/nci/camod/service/impl/ImageManagerImpl.java
@@ -1,7 +1,16 @@
 /*
- * $Id: ImageManagerImpl.java,v 1.23 2007-06-25 16:57:11 pandyas Exp $
+ * $Id: ImageManagerImpl.java,v 1.26 2007-08-01 18:11:46 pandyas Exp $
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.25  2007/08/01 18:05:02  pandyas
+ * VCDE changes
+ *
+ * Revision 1.24  2007/07/31 12:02:22  pandyas
+ * VCDE silver level  and caMOD 2.3 changes
+ *
+ * Revision 1.23  2007/06/25 16:57:11  pandyas
+ * Fixed code so staining_method will not clear when other fields are edited
+ *
  * Revision 1.22  2007/04/20 17:51:27  pandyas
  * Modified to add Staining Method tree to Image submission
  *
@@ -227,18 +236,21 @@ public class ImageManagerImpl extends BaseManager implements ImageManager {
 					String ftpPassword = camodProperties
 							.getProperty("caimage.ftp.password");
 
-					String ftpStorageDirectory = camodProperties
-							.getProperty("caimage.ftp.modelstoragedirectory");
-
-					// Determine which path to do the view in
+                    // Determine which path to use to store the image AND              
+					// Determine which path to store or use to view in 
+                    String ftpStorageDirectory = "";
 					String serverViewUrl = "";
 					if (inStorageDirKey
 							.equals(Constants.CaImage.FTPGENCONSTORAGEDIRECTORY)) {
 						serverViewUrl = camodProperties
 								.getProperty("caimage.genconview.uri");
+                        ftpStorageDirectory = camodProperties
+                        .getProperty("caimage.ftp.genconstoragedirectory");                         
 					} else {
 						serverViewUrl = camodProperties
 								.getProperty("caimage.modelview.uri");
+                        ftpStorageDirectory = camodProperties
+                        .getProperty("caimage.ftp.modelstoragedirectory");                        
 					}
 
 					// Upload the file to caIMAGE
@@ -272,10 +284,10 @@ public class ImageManagerImpl extends BaseManager implements ImageManager {
 								+ uniqueFileName;
 						String theViewUrl = Constants.CaImage.LEGACYJSP
 								+ theType + uniqueFileName;
-						inImage.setFileServerLocation(theThumbUrl
+						inImage.setUrl(theThumbUrl
 								+ Constants.CaImage.FILESEP + theViewUrl);
 					} else {
-						inImage.setFileServerLocation(serverViewUrl
+						inImage.setUrl(serverViewUrl
 								+ uniqueFileName);
 					}
 				} else {
