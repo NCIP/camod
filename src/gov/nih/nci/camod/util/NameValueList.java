33c9f34c4a6127e81e14da691174718df7003210
Sima Pandya <pandyas@mail.nih.gov>
2007-10-18 18:26:36 +0000

    Added to prevent cross--site scripting attacks
    SVN-Revision: 3210

--- src/gov/nih/nci/camod/util/NameValueList.java
+++ src/gov/nih/nci/camod/util/NameValueList.java
@@ -1,13 +1,60 @@
+/**
+ *   The caMOD Software License, Version 1.0
+ *
+ *   Copyright 2005-2006 SAIC. This software was developed in conjunction with the National Cancer
+ *   Institute, and so to the extent government employees are co-authors, any rights in such works
+ *   shall be subject to Title 17 of the United States Code, section 105.
+ *
+ *   Redistribution and use in source and binary forms, with or without modification, are permitted
+ *   provided that the following conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ *   and the disclaimer of Article 3, below.  Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ *   2.  The end-user documentation included with the redistribution, if any, must include the
+ *   following acknowledgment:
+ *
+ *   "This product includes software developed by the SAIC and the National Cancer
+ *   Institute."
+ *
+ *   If no such end-user documentation is to be included, this acknowledgment shall appear in the
+ *   software itself, wherever such third-party acknowledgments normally appear.
+ *
+ *   3. The names "The National Cancer Institute", "NCI" and "SAIC" must not be used to endorse or
+ *   promote products derived from this software.
+ *
+ *   4. This license does not authorize the incorporation of this software into any third party proprietary
+ *   programs.  This license does not authorize the recipient to use any trademarks owned by either
+ *   NCI or SAIC.
+ *
+ *
+ *   5. THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESSED OR IMPLIED
+ *   WARRANTIES, (INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE) ARE
+ *   DISCLAIMED.  IN NO EVENT SHALL THE NATIONAL CANCER INSTITUTE, SAIC, OR
+ *   THEIR AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ *   OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *   
+ * $Id: NameValueList.java,v 1.2 2007-10-18 18:26:36 pandyas Exp $
+ * 
+ * $Log: not supported by cvs2svn $
+ *
+ */
 package gov.nih.nci.camod.util;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class NameValueList
 {
-    // stores a list of PI Names Phases
-    private static List piNameList = new ArrayList();
-
     // stores a list of species
     private static List approvedSpeciesList = new ArrayList();
 
@@ -17,12 +64,8 @@ public class NameValueList
     // stores a list of carcinogenic interventions
     private static List carcinogenicInterventionList = new ArrayList();
 
-    // stores a list of agent names
-    private static List agentNameList = new ArrayList();
-
     // stores a list of external Sources
-    private static List externalSourceList = new ArrayList();
-    
+    private static List externalSourceList = new ArrayList();    
     
     
     
@@ -101,26 +144,6 @@ public class NameValueList
         return  carcinogenicInterventionList ;
     }   
     
-    public static void generatePiNameList() {
-
-        piNameList = new ArrayList();
-        piNameList.add(new NameValue("Chemical / Drug","Chemical / Drug"));
-        piNameList.add(new NameValue("Environment","Environment"));
-        piNameList.add(new NameValue("Growth Factor","Growth Factor")); 
-        piNameList.add(new NameValue("Hormone","Hormone"));
-        piNameList.add(new NameValue("Nutrition","Nutrition")); 
-        piNameList.add(new NameValue("Other","Other"));
-        piNameList.add(new NameValue("Radiation","Radiation")); 
-        piNameList.add(new NameValue("Viral","Viral"));
-        piNameList.add(new NameValue("Antibody","Antibody"));        
-        piNameList.add(new NameValue("Bacteria","Bacteria"));
-        piNameList.add(new NameValue("Plasmid","Plasmid")); 
-        piNameList.add(new NameValue("Transposon","Transposon"));
-        
-    }
-
-    public static List getPiNameList()  {
-        generatePiNameList();
-        return  piNameList ;
-    }    
+ 
+ 
 }
