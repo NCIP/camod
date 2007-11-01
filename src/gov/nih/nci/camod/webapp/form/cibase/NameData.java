2d53f01c4386d4cc9a2b123b809951643269169b
Sima Pandya <pandyas@mail.nih.gov>
2007-11-01 13:43:08 +0000

    Fixed #8355     Add comments field to every submission page
    SVN-Revision: 3295

--- src/gov/nih/nci/camod/webapp/form/cibase/NameData.java
+++ src/gov/nih/nci/camod/webapp/form/cibase/NameData.java
@@ -1,9 +1,12 @@
 /**
  * @author dgeorge
  * 
- * $Id: NameData.java,v 1.1 2005-09-28 13:58:45 georgeda Exp $
+ * $Id: NameData.java,v 1.2 2007-11-01 13:43:08 pandyas Exp $
  * 
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2005/09/28 13:58:45  georgeda
+ * More work on manager interface for CI
+ *
  * Revision 1.1  2005/09/27 19:17:08  georgeda
  * Refactor of CI managers
  *
@@ -24,4 +27,8 @@ public interface NameData  {
     public String getOtherName();
 
     public void setOtherName(String otherName);
+    
+	public String getComments();
+	
+	public void setComments(String comments);    
 }
