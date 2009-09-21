90e1b3cd1db92671d979e3e189ddf57d98721911
Sima Pandya <pandyas@mail.nih.gov>
2009-09-21 18:27:24 +0000

    reorganized test folders to eliminate extra levels
    SVN-Revision: 4669

---
+++ software/camod/test/unit/web/alltests/AllTests.java
@@ -0,0 +1,35 @@
+package unit.web.alltests;
+
+import unit.web.*;
+import unit.web.allwebtests.*;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+/**
+ * @author pandyas
+ */
+public class AllTests extends TestCase {
+
+	public AllTests(String arg0) {
+		super(arg0);
+	}
+	
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite();
+
+        // Submission tests
+        suite.addTest(AllSearchTests.suite());
+        suite.addTest(AllSubmissionTests.suite());
+
+        return suite;
+    }
+
+    public static void main(String args[])
+    {
+        TestRunner.run(suite());
+    }	
+
+}
