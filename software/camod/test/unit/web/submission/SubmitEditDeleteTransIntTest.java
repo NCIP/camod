9f88d8d657f1e9adbd223ab2075c2ad2e08baf57
Sima Pandya <pandyas@mail.nih.gov>
2009-09-21 18:28:19 +0000

    reorganized test folders to eliminate extra levels
    SVN-Revision: 4671

---
+++ software/camod/test/unit/web/submission/SubmitEditDeleteTransIntTest.java
@@ -0,0 +1,138 @@
+/**
+ * @author pandyas
+ * 
+ * $Id: SubmitEditDeleteTransIntTest.java,v 1.1 2009-07-13 17:45:12 pandyas Exp $
+ *
+ * $Log: not supported by cvs2svn $
+ * Revision 1.1  2009/07/07 17:46:54  pandyas
+ * modified according to the recommended directory layout for BDA
+ *
+ * Revision 1.4  2006/11/09 17:10:05  pandyas
+ * modified sucess, edit, and delete message to be used for both SiRNA and Morpholino.  Modified code to use only one manager so both messages must be generic.
+ *
+ * Revision 1.3  2006/10/18 18:11:06  pandyas
+ * modified form name from morpholino to transientInterference
+ *
+ * Revision 1.2  2006/10/17 17:23:00  pandyas
+ * changed name to match object
+ *
+ * Revision 1.1  2006/10/16 14:49:56  pandyas
+ * modified name of test to add SiRNA
+ *
+ * Revision 1.1  2006/05/03 20:06:16  pandyas
+ * Testing the new Morpholino object data in the application
+ *
+ * 
+ */
+package unit.web.submission;
+
+import java.util.ResourceBundle;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import unit.web.base.BaseModelNeededTest;
+import unit.web.util.TestUtil;
+import com.meterware.httpunit.*;
+
+public class SubmitEditDeleteTransIntTest extends BaseModelNeededTest {
+
+	public SubmitEditDeleteTransIntTest(String arg0) {
+		super(arg0);
+	}
+
+	protected void setUp() throws Exception {
+        ResourceBundle theBundle = ResourceBundle.getBundle("test");
+
+        String theUsername = theBundle.getString("username");
+        String thePassword = theBundle.getString("password");
+        
+        loginToApplication(theUsername, thePassword);
+        createModel();
+	}
+
+	protected void tearDown() throws Exception {
+        deleteModel();
+        logoutOfApplication();
+	}
+	
+    public static Test suite() {
+        TestSuite suite = new TestSuite(SubmitEditDeleteTransIntTest.class);
+        return suite;
+    }
+    
+    public void testMorpholino() throws Exception {
+    	navigateToModelForEditing(myModelName);
+        
+        // Find Morpholino link to Submit
+        WebLink theLink = myWebConversation.getCurrentPage()
+                .getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "Enter Morpholino");
+        WebResponse theCurrentPage = theLink.click(); 
+        assertCurrentPageContains("if source is not listed");
+        WebForm theForm = theCurrentPage.getFormWithName("transientInterferenceForm");
+        theForm.setParameter("targetedRegion", "ABCDEFG");
+        theCurrentPage = theForm.submit();
+        TestUtil.getTextOnPage(theCurrentPage, "Error: Bad or missing data", "* indicates a required field");
+        
+        assertCurrentPageContains("You have successfully added a Transient Interference to this model!");
+        
+        /* Find Morpholino link to Edit */
+        theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "ABCDEFG");        
+        assertNotNull("Unable to find link to edit the Morpholino", theLink);        
+        theCurrentPage = theLink.click();        
+        assertCurrentPageContains("if source is not listed");
+        theForm = theCurrentPage.getFormWithName("transientInterferenceForm");
+        theForm.setParameter("targetedRegion", "ABCDEFGHI"); 
+        theCurrentPage = theForm.submit();
+        TestUtil.getTextOnPage(theCurrentPage, "Error: Bad or missing data", "* indicates a required field");
+        
+        assertCurrentPageContains("You have successfully edited a Transient Interference.");      
+        
+        /* Find Morpholino link to Delete*/ 
+        theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "ABCDEFGHI");
+        assertNotNull("Unable to find link to delete the Morpholino", theLink);        
+        theCurrentPage = theLink.click();        
+        assertCurrentPageContains("if source is not listed");
+        theForm = theCurrentPage.getFormWithName("transientInterferenceForm");               
+        theForm.getSubmitButton( "submitAction", "Delete" ).click();  
+      
+        assertCurrentPageContains("You have successfully deleted a Transient Interference."); 
+    } 
+    
+    public void testSirna() throws Exception {
+        navigateToModelForEditing(myModelName);
+        
+        // Find Morpholino link to Submit
+        WebLink theLink = myWebConversation.getCurrentPage()
+                .getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "Enter SiRNA");
+        WebResponse theCurrentPage = theLink.click(); 
+        assertCurrentPageContains("if source is not listed");
+        WebForm theForm = theCurrentPage.getFormWithName("transientInterferenceForm");
+        theForm.setParameter("targetedRegion", "ABCDEFG");
+        theCurrentPage = theForm.submit();
+        TestUtil.getTextOnPage(theCurrentPage, "Error: Bad or missing data", "* indicates a required field");
+        
+        assertCurrentPageContains("You have successfully added a Transient Interference to this model!");
+        
+        // Find Morpholino link to Edit 
+        theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "ABCDEFG");        
+        assertNotNull("Unable to find link to edit the SiRNA", theLink);        
+        theCurrentPage = theLink.click();        
+        assertCurrentPageContains("if source is not listed");
+        theForm = theCurrentPage.getFormWithName("transientInterferenceForm");
+        theForm.setParameter("targetedRegion", "ABCDEFGHI"); 
+        theCurrentPage = theForm.submit();
+        TestUtil.getTextOnPage(theCurrentPage, "Error: Bad or missing data", "* indicates a required field");
+        
+        assertCurrentPageContains("You have successfully edited a Transient Interference.");      
+        
+        // Find Morpholino link to Delete 
+        theLink = myWebConversation.getCurrentPage().getFirstMatchingLink(WebLink.MATCH_CONTAINED_TEXT, "ABCDEFGHI");
+        assertNotNull("Unable to find link to delete the SiRNA", theLink);        
+        theCurrentPage = theLink.click();        
+        assertCurrentPageContains("if source is not listed");
+        theForm = theCurrentPage.getFormWithName("transientInterferenceForm");               
+        theForm.getSubmitButton( "submitAction", "Delete" ).click();  
+      
+        assertCurrentPageContains("You have successfully deleted a Transient Interference."); 
+    }     
+
+}
