2cb00937ccbdc3026eec28e4b5cdf12c8f5b5fef
Sima Pandya <pandyas@mail.nih.gov>
2009-10-15 17:12:40 +0000

    upgraded to final copy run on DEV DB.  Changed ";" to "/" to signal end of sql statement as per BDA instructions.
    SVN-Revision: 4800

--- software/camod/db/db-upgrade/oracle/2009_Oct_2_6_1.sql
+++ software/camod/db/db-upgrade/oracle/2009_Oct_2_6_1.sql
@@ -1,59 +1,50 @@
--- update records left blank by MTB data load script
 update ENVIRONMENTAL_FACTOR ef
 set ef.IS_INDUCED_MUTATION_TRIGGER = '0'
 where ef.IS_INDUCED_MUTATION_TRIGGER is null
 /
 
---  does not work yet
--- sub-select returns 4023 rows, I manually changed 7 rows on dev
+-- ran on dev manually - will automate in BDA (Fixed 1730 rows)
 update ENVIRONMENTAL_FACTOR ef
 set ef.TYPE = ef.TYPE_ALTERN_ENTRY
 where ef.TYPE is null
-and ef.ENVIRONMENTAL_FACTOR_ID is IN (
-select ef.ENVIRONMENTAL_FACTOR_ID
-from environmental_factor ef, abs_cancer_model ac, carcinogen_exposure ce
+and ef.ENVIRONMENTAL_FACTOR_ID IN (
+select ef.ENVIRONMENTAL_FACTOR_ID 
+From environmental_factor ef, abs_cancer_model ac, carcinogen_exposure ce 
 where ef.ENVIRONMENTAL_FACTOR_ID = ce.ENVIRONMENTAL_FACTOR_ID
 and ce.ABS_CANCER_MODEL_ID = ac.ABS_CANCER_MODEL_ID
-and ac.EXTERNAL_SOURCE = 'Jax MTB'
-order by ef.TYPE, ef.TYPE_ALTERN_ENTRY)
+and ac.EXTERNAL_SOURCE = 'Jax MTB')
 /
 
-
--- then null out the typeAlternEntry field
+-- then null out the typeAlternEntry field (Fixed 1730 rows)
 update ENVIRONMENTAL_FACTOR ef
 set ef.TYPE_ALTERN_ENTRY = null
-where ef.ENVIRONMENTAL_FACTOR_ID is IN (
-select ef.ENVIRONMENTAL_FACTOR_ID
-from environmental_factor ef, abs_cancer_model ac, carcinogen_exposure ce
+where ef.ENVIRONMENTAL_FACTOR_ID IN (
+select ef.ENVIRONMENTAL_FACTOR_ID 
+From environmental_factor ef, abs_cancer_model ac, carcinogen_exposure ce 
 where ef.ENVIRONMENTAL_FACTOR_ID = ce.ENVIRONMENTAL_FACTOR_ID
 and ce.ABS_CANCER_MODEL_ID = ac.ABS_CANCER_MODEL_ID
-and ac.EXTERNAL_SOURCE = 'Jax MTB'
-order by ef.TYPE, ef.TYPE_ALTERN_ENTRY
+and ac.EXTERNAL_SOURCE = 'Jax MTB')
 /
 
--- repeat for name and nameAlternEntry fields
--- sub-select returns 4023 rows, I manually changed 7 rows on dev
+-- repeat for name and nameAlternEntry fields (Fixed 1723 rows)
 update ENVIRONMENTAL_FACTOR ef
-set ef.name = ef.name_ALTERN_ENTRY
-where ef.name is null
-and ef.ENVIRONMENTAL_FACTOR_ID is IN (
-select ef.ENVIRONMENTAL_FACTOR_ID
-from environmental_factor ef, abs_cancer_model ac, carcinogen_exposure ce
+set ef.NAME = ef.NAME_ALTERN_ENTRY
+where ef.NAME is null
+and ef.ENVIRONMENTAL_FACTOR_ID IN (
+select ef.ENVIRONMENTAL_FACTOR_ID 
+From environmental_factor ef, abs_cancer_model ac, carcinogen_exposure ce 
 where ef.ENVIRONMENTAL_FACTOR_ID = ce.ENVIRONMENTAL_FACTOR_ID
 and ce.ABS_CANCER_MODEL_ID = ac.ABS_CANCER_MODEL_ID
-and ac.EXTERNAL_SOURCE = 'Jax MTB'
-order by ef.TYPE, ef.TYPE_ALTERN_ENTRY)
+and ac.EXTERNAL_SOURCE = 'Jax MTB')
 /
 
-
--- then null out the nameAlternEntry field
+-- then null out the typeAlternEntry field (Fixed 1730 rows)
 update ENVIRONMENTAL_FACTOR ef
-set ef.name_ALTERN_ENTRY = null
-where ef.ENVIRONMENTAL_FACTOR_ID is IN (
-select ef.ENVIRONMENTAL_FACTOR_ID
-from environmental_factor ef, abs_cancer_model ac, carcinogen_exposure ce
+set ef.NAME_ALTERN_ENTRY = null
+where ef.ENVIRONMENTAL_FACTOR_ID IN (
+select ef.ENVIRONMENTAL_FACTOR_ID 
+From environmental_factor ef, abs_cancer_model ac, carcinogen_exposure ce 
 where ef.ENVIRONMENTAL_FACTOR_ID = ce.ENVIRONMENTAL_FACTOR_ID
 and ce.ABS_CANCER_MODEL_ID = ac.ABS_CANCER_MODEL_ID
-and ac.EXTERNAL_SOURCE = 'Jax MTB'
-order by ef.TYPE, ef.TYPE_ALTERN_ENTRY
-/
+and ac.EXTERNAL_SOURCE = 'Jax MTB')
+/ 
