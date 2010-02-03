bc97107375e529f3052d2f83d74537ab45fb9f9d
Sima Pandya <pandyas@mail.nih.gov>
2010-02-03 01:28:16 +0000

    added code for security scan - clean all headers
    SVN-Revision: 5150

--- software/camod/src/gov/nih/nci/camod/webapp/action/ViewTOCSearchResultsAction.java
+++ software/camod/src/gov/nih/nci/camod/webapp/action/ViewTOCSearchResultsAction.java
@@ -43,11 +43,8 @@ package gov.nih.nci.camod.webapp.action;
 
 import gov.nih.nci.camod.Constants;
 import gov.nih.nci.camod.service.impl.*;
-import gov.nih.nci.camod.util.CriteriaTableUtil;
 import gov.nih.nci.camod.util.NameValueList;
 import gov.nih.nci.camod.util.SafeHTMLUtil;
-import gov.nih.nci.camod.webapp.form.SearchForm;
-
 import java.util.Enumeration;
 import java.util.List;
 
@@ -60,29 +57,24 @@ public class ViewTOCSearchResultsAction extends BaseAction {
 
     public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
             HttpServletResponse response) throws Exception {
-
-        log.info("In ViewTOCSearchResultsAction.execute");
+    	
+    	log.info("In ViewTOCSearchResultsAction.execute");
+    	
+        // Clean all headers for security scan (careful about what chars you allow)
+    	String headername = "";
+    	for(Enumeration e = request.getHeaderNames(); e.hasMoreElements();){
+    		headername = (String)e.nextElement();
+    		log.info("ViewTOCSearchResultsAction headername: " + headername);
+    		String cleanHeaders = SafeHTMLUtil.clean(headername);
+    		log.info("ViewTOCSearchResultsAction cleaned headername: " + headername);
+    	}    	
         
         String theForward = "next";
         
+        // check for TOC query names inside try
         String theKey = (String) request.getParameter(Constants.Parameters.TOCQUERYKEY);
         log.info("theKey: " + theKey);
-        
-        // clean specific header that is used in SQL injection
-        if (request.getHeader("X-Forwarded-For") != null){
-        	String sID = request.getHeader("X-Forwarded-For");
-            log.debug("sID: " + sID);
-            sID = SafeHTMLUtil.clean(sID);
-        }
-       
-        // get all header names and clean them to prevent SQL injection
-        Enumeration headerNames = request.getHeaderNames();
-        while(headerNames.hasMoreElements()){
-        	String sID = headerNames.nextElement().toString();
-            log.debug("sID: " + sID);
-            sID = SafeHTMLUtil.clean(sID);
-        	log.debug("cleaned header: " + sID);            	
-        }        
+  
 
         try {
             
@@ -101,7 +93,7 @@ public class ViewTOCSearchResultsAction extends BaseAction {
 	                // set theForward to failure - fail gracefully but do not let query continue
 	                theForward = "failure";
 	            } else {	            
-		            log.info("theKey is a valid value - continue with querry: " + theKey); 
+		            log.debug("theKey is a valid value - continue with querry: " + theKey); 
             
 		            // Handle external linkage
 		            if (request.getSession().getAttribute(Constants.TOCSearch.TOC_QUERY_RESULTS) == null) {
@@ -111,12 +103,12 @@ public class ViewTOCSearchResultsAction extends BaseAction {
 		                        + Constants.TOCSearch.TOC_QUERY_FILE);
 		
 		                List theResults = theTOCManager.process();
-		                log.info("TOC: " + theResults); 
+		                log.debug("TOC: " + theResults); 
 		                request.getSession().setAttribute(Constants.TOCSearch.TOC_QUERY_RESULTS, theResults);		                
 		            }
 
 		            List theGroupList = (List) request.getSession().getAttribute(Constants.TOCSearch.TOC_QUERY_RESULTS);
-		            log.info("theGroupList: " + theGroupList); 
+		            log.debug("theGroupList: " + theGroupList); 
 
 		            for (int i = 0; i < theGroupList.size(); i++) {
 		                TOCQueryGroup theQueryGroup = (TOCQueryGroup) theGroupList.get(i);
@@ -126,7 +118,7 @@ public class ViewTOCSearchResultsAction extends BaseAction {
 		
 		                    if (theQuery.getKey().equals(theKey)) {                   	
 		                        request.getSession().setAttribute(Constants.SEARCH_RESULTS, theQuery.getResults());
-		                        log.info("TOC theQuery.getResults(): " + theQuery.getResults());
+		                        log.debug("TOC theQuery.getResults(): " + theQuery.getResults());
 		                        break;
 		                    }
 		                }
@@ -136,14 +128,14 @@ public class ViewTOCSearchResultsAction extends BaseAction {
         } catch (Exception e) {
 
             theForward = "failure";
-            log.info("Caught an error running the canned query: ", e);
+            log.debug("Caught an error running the canned query: ", e);
 
             // Encountered an error saving the model.
             ActionMessages theMsg = new ActionMessages();
             theMsg.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("errors.admin.message"));
             saveErrors(request, theMsg);
         }
-        log.info("Exiting ViewTOCSearchResultsAction theForward: "+ theForward);
+        log.debug("Exiting ViewTOCSearchResultsAction theForward: "+ theForward);
         return mapping.findForward(theForward);
     }
     
