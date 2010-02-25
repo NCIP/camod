9ce823cefc3be0b517edb33774703ed84a778b99
Sima Pandya <pandyas@mail.nih.gov>
2010-02-25 17:00:36 +0000

    cleaned Referer and X-Forwarded-For headers to prevent future security scan failure
    SVN-Revision: 5245

--- software/camod/src/gov/nih/nci/camod/webapp/action/AdvancedSearchPopulateAction.java
+++ software/camod/src/gov/nih/nci/camod/webapp/action/AdvancedSearchPopulateAction.java
@@ -91,6 +91,21 @@ public class AdvancedSearchPopulateAction extends BaseAction {
     		String cleanHeaders = SafeHTMLUtil.clean(headername);
     		log.debug("AdvancedSearchPopulateAction cleaned headername: " + headername);
     	}
+    	
+    	// get and clean header to prevent SQL injection
+       	String sID = null;
+        if (request.getHeader("X-Forwarded-For") != null){
+        	sID = request.getHeader("X-Forwarded-For");
+            log.info("cleaned X-Forwarded-For: " + sID);
+            sID = SafeHTMLUtil.clean(sID);
+        }
+        
+    	// get and clean header to prevent SQL injection
+        if (request.getHeader("Referer") != null){
+        	sID = request.getHeader("Referer");
+            log.info("cleaned Referer: " + sID);
+            sID = SafeHTMLUtil.clean(sID);
+        }       	
 
         // Reset the non-simple-search options
         SearchForm theSearchForm = (SearchForm) form;
