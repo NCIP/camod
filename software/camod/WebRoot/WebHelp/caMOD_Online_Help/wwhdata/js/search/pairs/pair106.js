6f8d1055f9406417310a9c10ebe29e2902bbeadb
Sima Pandya <pandyas@mail.nih.gov>
2010-08-02 18:51:12 +0000

    restored version 2.6 online help from original files
    SVN-Revision: 5537

---
+++ software/camod/WebRoot/WebHelp/caMOD_Online_Help/wwhdata/js/search/pairs/pair106.js
@@ -0,0 +1,25 @@
+function FileData_Pairs(x)
+{
+x.t("camod","user");
+x.t("camod","coordinator");
+x.t("camod","administration");
+x.t("user","camod");
+x.t("user","assigned");
+x.t("user","management");
+x.t("coordinator","information");
+x.t("assignment","camod");
+x.t("assignment","besides");
+x.t("assignment","roles");
+x.t("registered","user");
+x.t("simply","registered");
+x.t("assigned","roles");
+x.t("information","administration");
+x.t("besides","simply");
+x.t("administration","user");
+x.t("administration","roles");
+x.t("management","roles");
+x.t("roles","camod");
+x.t("roles","assignment");
+x.t("roles","management");
+x.t("roles","roles");
+}
---
+++ software/camod/WebRoot/WebHelp/caMOD_Online_Help/wwhdata/js/search/pairs/pair98.js
@@ -0,0 +1,25 @@
+function FileData_Pairs(x)
+{
+x.t("camod","administration");
+x.t("reject","comment");
+x.t("reject","record");
+x.t("overview","screener");
+x.t("tasks","scan");
+x.t("perform","following");
+x.t("completed","records");
+x.t("records","appropriateness");
+x.t("record","scan");
+x.t("functions","overview");
+x.t("comments","appropriateness");
+x.t("scan","comments");
+x.t("scan","submitted");
+x.t("appropriateness","approve");
+x.t("submitted","completed");
+x.t("administration","functions");
+x.t("administration","administration");
+x.t("following","tasks");
+x.t("screener","camod");
+x.t("screener","perform");
+x.t("screener","screener");
+x.t("approve","reject");
+}
