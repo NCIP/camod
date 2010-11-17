bc883c4ab8565a0be09b7d34a1126e1870eee41e
Sima Pandya <pandyas@mail.nih.gov>
2010-11-17 03:05:54 +0000

    JIRA# CAMOD-969  	 Retrieve organ information for search results pages from DB not EVS Remove stage information in Zebrafish tissue entries in caMOD
    SVN-Revision: 5654

--- software/camod/db/db-upgrade/oracle/camod_upgrade_2.7_1.sql
+++ software/camod/db/db-upgrade/oracle/camod_upgrade_2.7_1.sql
@@ -3,6 +3,26 @@ update organ o
 set o.NAME=replace(o.NAME, ' (MMHCC)', '')
 where o.NAME LIKE '%(MMHCC)';
 
+update organ o
+set o.NAME=replace(o.NAME, ' (Segmentation:20-25 somites to Adult)', '')
+where o.NAME LIKE '%(Segmentation:20-25 somites to Adult)';
+
+update organ o
+set o.NAME=replace(o.NAME, ' (Segmentation:1-4 somites to Adult)', '')
+where o.NAME LIKE '%(Segmentation:1-4 somites to Adult)';
+
+update organ o
+set o.NAME=replace(o.NAME, ' (Unknown to Adult)', '')
+where o.NAME LIKE '%(Unknown to Adult)';
+
+update organ o
+set o.NAME=replace(o.NAME, ' (Zygote:1-cell to Adult)', '')
+where o.NAME LIKE '%(Zygote:1-cell to Adult)';
+
+update organ o
+set o.NAME=replace(o.NAME, ' (Blastula:High to Adult)', '')
+where o.NAME LIKE '%(Blastula:High to Adult)';
+
 
 commit;
 
