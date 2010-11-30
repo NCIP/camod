98b98ed1b79068013bda38af346dd8997c5e3cbd
Sima Pandya <pandyas@mail.nih.gov>
2010-11-30 03:43:46 +0000

    added SQl for JIRA#  Bug  	 CAMOD-974  	 Metastases are gone from earlier models
    SVN-Revision: 5675

--- software/camod/db/db-upgrade/oracle/camod_upgrade_2.7_2.sql
+++ software/camod/db/db-upgrade/oracle/camod_upgrade_2.7_2.sql
@@ -2,11 +2,17 @@
 -- JIRA #CAMOD-974  	 Metastases are gone from earlier models
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 130 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=131;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 130 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=133;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 130 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=134;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 130 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=135;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 130 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=136;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 130 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=137;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 130 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=138;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 693 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=694;
@@ -20,18 +26,25 @@ update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 830 where h.PARENT_HIST
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 854 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=855;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 864 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=865;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 864 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=866;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 864 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=867;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 864 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=868;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 850 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=874;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 850 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=875;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 850 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=876;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 850 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=877;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 787 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=893;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 898 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=902;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 898 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=903;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 778 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=905;
@@ -47,6 +60,7 @@ update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1121 where h.PARENT_HIS
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1169 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1170;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1430 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1431;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1430 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1432;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1441 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1442;
@@ -56,105 +70,79 @@ update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1548 where h.PARENT_HIS
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1562 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1563;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1564 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1573;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1564 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1574;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1564 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1575;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1564 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1577;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1564 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1578;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1579 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1580;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1579 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1581;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1579 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1582;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1579 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1583;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1579 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1584;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1598 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1602;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1670 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1671;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1670 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1672;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1748 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1749;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1793 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1796;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1794 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1797;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1795 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1798;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1834 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1841;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1834 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1842;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1834 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1843;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1891 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1892;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 1891 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=1893;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 2064 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=2065;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 2064 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=2066;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 2064 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=2067;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 2064 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=2068;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 2064 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=2069;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 2070 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=2074;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 2070 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=2075;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 2161 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=2162;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 2161 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=2163;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 2161 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=2164;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 2284 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=2285;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 2410 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=2413;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 2410 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=2414;
 
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 2405 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=2416;
+
 update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 2405 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=2417;
 
 
--- Matches were made based on the close promiximity of the histopathology Ids and the context of the model entry 
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 10008866 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=10008869;
-
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 10008884 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=10008888;
-
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 10008929 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=10008932;
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 10008929 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=10008934;
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 10008929 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=10008936;
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 10008929 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=10008938;
-
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 10009096 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=10009098;
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 10009096 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=10009099;
-
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 10009197 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=10009198;
-
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 10009658 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=10009665;
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 10009658 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=10009666;
-
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 10008492 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=10009799;
-
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 10009993 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=10009995;
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 10009993 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=10009997;
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 10009993 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=10009998;
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 10009993 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=10009999;
-
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 10010710 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=10010711;
-
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 50061843 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=50061844;
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 50061843 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=50061845;
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 50061843 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=50061846;
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 50061843 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=50061847;
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 50061843 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=50061848;
-
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 50061843 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=50061848;
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 50061843 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=50061848;
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 50061843 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=50061848;
-
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID =50052646  where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=50052649;
-
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 50052657 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id= 50052660;
-
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID =50052661  where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=50052663;
-
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 50052475 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id= 50052479;
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 50052475 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id= 50052482;
-
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 50052393  where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id= 50052396;
-
-update histopathology h set h.PARENT_HISTOPATHOLOGY_ID = 50050679 where h.PARENT_HISTOPATHOLOGY_ID is null and h.histopathology_id=50050680 ;
 
 commit;
