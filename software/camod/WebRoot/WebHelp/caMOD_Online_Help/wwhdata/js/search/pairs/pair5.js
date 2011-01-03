2d88f956ec13bc549b8b08e39f93d5d6ceaf76f9
Sima Pandya <pandyas@mail.nih.gov>
2011-01-03 00:40:32 +0000

    uploaded version 2.7 files
    SVN-Revision: 5718

---
+++ software/camod/WebRoot/WebHelp/caMOD_Online_Help/wwhdata/js/search/pairs/pair5.js
@@ -0,0 +1,25 @@
+function FileData_Pairs(x)
+{
+x.t("camod","camod");
+x.t("camod","fall");
+x.t("camod","welcome");
+x.t("user","account");
+x.t("user","registered");
+x.t("categories","guest");
+x.t("new","user");
+x.t("getting","started");
+x.t("fall","two");
+x.t("creating","new");
+x.t("account","getting");
+x.t("account","creating");
+x.t("account","users");
+x.t("tasks","creating");
+x.t("welcome","login");
+x.t("started","camod");
+x.t("registered","user");
+x.t("guest","user");
+x.t("login","page");
+x.t("page","tasks");
+x.t("users","camod");
+x.t("two","categories");
+}
---
+++ software/camod/WebRoot/WebHelp/caMOD_Online_Help/wwhdata/js/search/pairs/pair75.js
@@ -0,0 +1,24 @@
+function FileData_Pairs(x)
+{
+x.t("included","camod");
+x.t("camod","working");
+x.t("background","entering");
+x.t("working","animal");
+x.t("editing","sirna");
+x.t("editing","models");
+x.t("transient","interference");
+x.t("details","page");
+x.t("entering","editing");
+x.t("sirna","background");
+x.t("sirna","details");
+x.t("sirna","sirna");
+x.t("sirna","submitting");
+x.t("sirna","information");
+x.t("submitting","editing");
+x.t("interference","sirna");
+x.t("models","transient");
+x.t("models","information");
+x.t("page","included");
+x.t("information","sirna");
+x.t("animal","models");
+}
